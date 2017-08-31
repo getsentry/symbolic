f1e17aa95c98322f120d222be17a49035b0af207
Armin Ronacher <armin.ronacher@active-4.com>
2017-09-01 02:12:51 +0500

    feat: add minidump to main crate

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,11 @@
 #[doc(hidden)] pub extern crate symbolic_proguard;
 #[doc(hidden)] pub extern crate symbolic_sourcemap;
 #[doc(hidden)] pub extern crate symbolic_demangle;
+#[doc(hidden)] pub extern crate symbolic_minidump;
 #[doc(hidden)] pub extern crate symbolic_common;
 
 pub use symbolic_proguard as proguard;
 pub use symbolic_proguard as sourcemap;
 pub use symbolic_demangle as demangle;
+pub use symbolic_minidump as minidump;
 pub use symbolic_common as common;
