e6c24e415fe1f192df852a3c6dfa91bdce5578ed
Armin Ronacher <armin.ronacher@active-4.com>
2017-09-01 02:38:12 +0500

    test: added tests for demangling

---
+++ demangle/tests/basic.rs
@@ -0,0 +1,38 @@
+extern crate symbolic_demangle;
+
+use symbolic_demangle::{demangle, DemangleOptions, DemangleFormat};
+
+
+fn assert_mangle(input: &str, output: Option<&str>, opts: DemangleOptions) {
+    let rv = demangle(input, &opts);
+    if let Some(rv) = demangle(input, &opts).unwrap() {
+        assert_eq!(Some(rv.as_str()), output);
+    } else {
+        assert_eq!(None, output);
+    }
+}
+
+
+#[test]
+fn test_rust_demangle() {
+    assert_mangle("_ZN3foo3barE", Some("foo::bar"), Default::default());
+}
+
+#[test]
+fn test_cpp_demangle() {
+    assert_mangle("__Z28JS_GetPropertyDescriptorByIdP9JSContextN2JS6HandleIP8JSObjectEENS2_I4jsidEENS1_13MutableHandleINS1_18PropertyDescriptorEEE",
+                  Some("JS_GetPropertyDescriptorById"), Default::default());
+    assert_mangle("__Z28JS_GetPropertyDescriptorByIdP9JSContextN2JS6HandleIP8JSObjectEENS2_I4jsidEENS1_13MutableHandleINS1_18PropertyDescriptorEEE",
+                  Some("JS_GetPropertyDescriptorById(JSContext*, JS::Handle<JSObject*>, JS::Handle<jsid>, JS::MutableHandle<JS::PropertyDescriptor>)"), DemangleOptions {
+        format: DemangleFormat::Full,
+        ..Default::default()
+    });
+}
+
+#[test]
+fn test_no_match() {
+    assert_mangle("_ZN3foo3barE", None, DemangleOptions {
+        languages: vec![],
+        ..Default::default()
+    });
+}
