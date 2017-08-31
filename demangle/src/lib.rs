6a0104f65550fca38f9981bb9c226adb657825aa
Armin Ronacher <armin.ronacher@active-4.com>
2017-09-01 02:53:22 +0500

    ref: refactor demangle and add missing docs

--- demangle/src/lib.rs
+++ demangle/src/lib.rs
@@ -1,26 +1,39 @@
 //! Provides demangling support.
+//!
+//! Currently supported languages:
+//!
+//! * C++
+//! * Rust
 extern crate symbolic_common;
 extern crate rustc_demangle;
 extern crate cpp_demangle;
 
 use symbolic_common::{ErrorKind, Result};
 
+/// Supported programming languages for demangling
 #[derive(Eq, PartialEq, Debug, Copy, Clone)]
 pub enum Language {
     Cpp,
-    Swift,
     Rust,
 }
 
+/// Defines the output format of the demangler
 #[derive(Eq, PartialEq, Debug, Copy, Clone)]
 pub enum DemangleFormat {
     Short,
     Full,
 }
 
+/// Options for the demangling
 #[derive(Debug, Clone)]
 pub struct DemangleOptions {
+    /// format to use for the output
     pub format: DemangleFormat,
+    /// languages that should be attempted for demangling
+    ///
+    /// These languages are tried in the order defined.  This is releavant
+    /// as some mangling schemes overlap for some trivial cases (for
+    /// instance rust and C++).
     pub languages: Vec<Language>,
 }
 
@@ -28,45 +41,60 @@ impl Default for DemangleOptions {
     fn default() -> DemangleOptions {
         DemangleOptions {
             format: DemangleFormat::Short,
-            languages: vec![Language::Cpp, Language::Swift, Language::Rust],
+            languages: vec![Language::Cpp, Language::Rust],
         }
     }
 }
 
+fn try_demangle_cpp(ident: &str, opts: &DemangleOptions) -> Result<Option<String>> {
+    if &ident[..2] != "_Z" {
+        return Ok(None);
+    }
+    match cpp_demangle::Symbol::new(ident) {
+        Ok(sym) => {
+            Ok(sym.demangle(&match opts.format {
+                DemangleFormat::Short => {
+                    cpp_demangle::DemangleOptions {
+                        no_params: true,
+                    }
+                },
+                DemangleFormat::Full => {
+                    cpp_demangle::DemangleOptions {
+                        no_params: false,
+                    }
+                }
+            }).ok())
+        }
+        Err(err) => {
+            Err(ErrorKind::BadSymbol(err.to_string()).into())
+        }
+    }
+}
+
+fn try_demangle_rust(ident: &str, opts: &DemangleOptions) -> Result<Option<String>> {
+    if let Ok(dm) = rustc_demangle::try_demangle(ident) {
+        Ok(Some(format!("{:#}", dm)))
+    } else {
+        Ok(None)
+    }
+}
+
 /// Demangles an identifier.
+///
+/// Example:
+///
+/// ```
+/// # use symbolic_demangle::*;
+/// let rv = demangle("_ZN3foo3barE", &Default::default()).unwrap();
+/// assert_eq!(&rv.unwrap(), "foo::bar");
+/// ```
 pub fn demangle(ident: &str, opts: &DemangleOptions) -> Result<Option<String>> {
     for &lang in &opts.languages {
-        match lang {
-            Language::Cpp => {
-                if &ident[..2] != "_Z" {
-                    continue;
-                }
-                match cpp_demangle::Symbol::new(ident) {
-                    Ok(sym) => {
-                        return Ok(sym.demangle(&match opts.format {
-                            DemangleFormat::Short => {
-                                cpp_demangle::DemangleOptions {
-                                    no_params: true,
-                                }
-                            },
-                            DemangleFormat::Full => {
-                                cpp_demangle::DemangleOptions {
-                                    no_params: false,
-                                }
-                            }
-                        }).ok())
-                    }
-                    Err(err) => {
-                        return Err(ErrorKind::BadSymbol(err.to_string()).into());
-                    }
-                }
-            },
-            Language::Swift => {},
-            Language::Rust => {
-                if let Ok(dm) = rustc_demangle::try_demangle(ident) {
-                    return Ok(Some(format!("{:#}", dm)));
-                }
-            }
+        if let Some(rv) = match lang {
+            Language::Cpp => try_demangle_cpp(ident, opts)?,
+            Language::Rust => try_demangle_rust(ident, opts)?,
+        } {
+            return Ok(Some(rv));
         }
     }
     Ok(None)
