d3f2add3bdc7d3233268029d9cf73c6b45fe6c5b
Armin Ronacher <armin.ronacher@active-4.com>
2017-09-11 11:16:36 +0200

    feat: Support arbitrary architectures

--- common/src/types.rs
+++ common/src/types.rs
@@ -3,13 +3,15 @@ use std::fmt;
 use errors::{ErrorKind, Result};
 
 /// An enum of supported architectures.
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
+#[allow(non_camel_case_types)]
 pub enum Arch {
     X86,
     X86_64,
     Arm64,
     ArmV7,
     ArmV7f,
+    Other(String),
 }
 
 impl Arch {
@@ -21,7 +23,15 @@ impl Arch {
             "arm64" => Arm64,
             "armv7" => ArmV7,
             "armv7f" => ArmV7f,
-            _ => return Err(ErrorKind::ParseError("unknown architecture").into()),
+            _ => {
+                let mut tokens = string.split_whitespace();
+                if let Some(tok) = tokens.next() {
+                    if tokens.next().is_none() {
+                        return Ok(Other(tok.into()))
+                    }
+                }
+                return Err(ErrorKind::ParseError("unknown architecture").into());
+            }
         })
     }
 }
@@ -35,6 +45,7 @@ impl fmt::Display for Arch {
             Arm64 => "arm64",
             ArmV7 => "armv7",
             ArmV7f => "armv7f",
+            Other(ref s) => s.as_str(),
         })
     }
 }
