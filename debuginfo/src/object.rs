df1f0d8bd242336d933f28e8cf84034727dfdc08
Jan Michael Auer <jan.auer@sentry.io>
2017-09-11 16:16:20 +0200

    ref(debuginfo): Rename variants() to objects()

--- debuginfo/src/object.rs
+++ debuginfo/src/object.rs
@@ -5,8 +5,7 @@ use std::io::Cursor;
 use uuid;
 use goblin;
 
-use symbolic_common::{ErrorKind, Result, ByteView, Arch};
-
+use symbolic_common::{Arch, ByteView, ErrorKind, Result};
 
 enum FatObjectKind<'a> {
     Elf(goblin::elf::Elf<'a>),
@@ -52,15 +51,9 @@ impl<'a> FatObject<'a> {
             let buf = &byteview;
             let mut cur = Cursor::new(buf);
             match goblin::peek(&mut cur)? {
-                goblin::Hint::Elf(_) => {
-                    FatObjectKind::Elf(goblin::elf::Elf::parse(buf)?)
-                }
-                goblin::Hint::Mach(_) => {
-                    FatObjectKind::MachO(goblin::mach::Mach::parse(buf)?)
-                }
-                goblin::Hint::MachFat(_) => {
-                    FatObjectKind::MachO(goblin::mach::Mach::parse(buf)?)
-                }
+                goblin::Hint::Elf(_) => FatObjectKind::Elf(goblin::elf::Elf::parse(buf)?),
+                goblin::Hint::Mach(_) => FatObjectKind::MachO(goblin::mach::Mach::parse(buf)?),
+                goblin::Hint::MachFat(_) => FatObjectKind::MachO(goblin::mach::Mach::parse(buf)?),
                 _ => {
                     return Err(ErrorKind::UnsupportedObjectFile.into());
                 }
@@ -73,40 +66,38 @@ impl<'a> FatObject<'a> {
     }
 
     /// Returns a list of variants.
-    pub fn variants(&'a self) -> Result<Vec<Object<'a>>> {
+    pub fn objects(&'a self) -> Result<Vec<Object<'a>>> {
         let mut rv = vec![];
         match self.kind {
             FatObjectKind::Elf(ref elf) => {
                 rv.push(Object {
                     fat_object: self,
-                    // TODO: fix me
-                    arch: Arch::X86_64,
+                    arch: Arch::X86_64, // TODO(ja): fix me
                     target: ObjectTarget::Elf(elf),
                 });
             }
-            FatObjectKind::MachO(ref mach) => {
-                match *mach {
-                    goblin::mach::Mach::Fat(ref fat) => {
-                        for (idx, arch) in fat.iter_arches().enumerate() {
-                            let arch = arch?;
-                            rv.push(Object {
-                                fat_object: self,
-                                arch: Arch::from_mach(arch.cputype as u32,
-                                                      arch.cpusubtype as u32)?,
-                                target: ObjectTarget::MachOFat(fat.get(idx)?),
-                            });
-                        }
-                    }
-                    goblin::mach::Mach::Binary(ref macho) => {
+            FatObjectKind::MachO(ref mach) => match *mach {
+                goblin::mach::Mach::Fat(ref fat) => {
+                    for (idx, arch) in fat.iter_arches().enumerate() {
+                        let arch = arch?;
                         rv.push(Object {
                             fat_object: self,
-                            arch: Arch::from_mach(macho.header.cputype as u32,
-                                                  macho.header.cpusubtype as u32)?,
-                            target: ObjectTarget::MachOBin(macho),
+                            arch: Arch::from_mach(arch.cputype as u32, arch.cpusubtype as u32)?,
+                            target: ObjectTarget::MachOFat(fat.get(idx)?),
                         });
                     }
                 }
-            }
+                goblin::mach::Mach::Binary(ref macho) => {
+                    rv.push(Object {
+                        fat_object: self,
+                        arch: Arch::from_mach(
+                            macho.header.cputype as u32,
+                            macho.header.cpusubtype as u32,
+                        )?,
+                        target: ObjectTarget::MachOBin(macho),
+                    });
+                }
+            },
         }
         Ok(rv)
     }
