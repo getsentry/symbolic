12abdf84fb22d80c1b630bd817469c2409abe5f4
Armin Ronacher <armin.ronacher@active-4.com>
2017-09-11 13:47:21 +0200

    feat: Initial work on exposing variants

--- debuginfo/src/object.rs
+++ debuginfo/src/object.rs
@@ -5,11 +5,22 @@ use std::io::Cursor;
 use goblin;
 
 use symbolic_common::{ErrorKind, Result, ByteView};
+use symbolic_common::Arch;
 
 
 enum ObjectKind<'a> {
-    Elf((goblin::elf::Elf<'a>)),
-    MachO(goblin::mach::MachO<'a>),
+    Elf(goblin::elf::Elf<'a>),
+    MachO(goblin::mach::Mach<'a>),
+}
+
+enum VariantTarget<'a> {
+    MachOBin(&'a goblin::mach::MachO<'a>),
+    MachOFat(goblin::mach::fat::FatArch),
+}
+
+pub struct Variant<'a> {
+    arch: Arch,
+    target: VariantTarget<'a>,
 }
 
 /// Represents an object file.
@@ -19,6 +30,7 @@ pub struct ObjectFile<'a> {
 }
 
 impl<'a> ObjectFile<'a> {
+    /// Provides a view to an object file from a byteview.
     pub fn new(byteview: &'a ByteView<'a>) -> Result<ObjectFile<'a>> {
         let kind = {
             let buf = &byteview;
@@ -28,7 +40,7 @@ impl<'a> ObjectFile<'a> {
                     ObjectKind::Elf(goblin::elf::Elf::parse(buf)?)
                 }
                 goblin::Hint::Mach(_) => {
-                    ObjectKind::MachO(goblin::mach::MachO::parse(buf, 0)?)
+                    ObjectKind::MachO(goblin::mach::Mach::parse(buf)?)
                 }
                 _ => {
                     return Err(ErrorKind::UnsupportedObjectFile.into());
@@ -39,5 +51,37 @@ impl<'a> ObjectFile<'a> {
             byteview: byteview,
             kind: kind,
         })
+    }
+
+    /// Returns a list of variants.
+    pub fn variants(&'a self) -> Result<Vec<Variant<'a>>> {
+        let mut rv = vec![];
+        match self.kind {
+            ObjectKind::Elf(ref elf) => {
+                return Err(ErrorKind::UnsupportedObjectFile.into());
+            }
+            ObjectKind::MachO(ref mach) => {
+                match *mach {
+                    goblin::mach::Mach::Fat(ref fat) => {
+                        for arch in fat.iter_arches() {
+                            let arch = arch?;
+                            rv.push(Variant {
+                                arch: Arch::from_mach(arch.cputype as u32,
+                                                      arch.cpusubtype as u32)?,
+                                target: VariantTarget::MachOFat(arch),
+                            });
+                        }
+                    }
+                    goblin::mach::Mach::Binary(ref macho) => {
+                        rv.push(Variant {
+                            arch: Arch::from_mach(macho.header.cputype as u32,
+                                                  macho.header.cpusubtype as u32)?,
+                            target: VariantTarget::MachOBin(macho),
+                        });
+                    }
+                }
+            }
+        }
+        Ok(rv)
     }
 }
