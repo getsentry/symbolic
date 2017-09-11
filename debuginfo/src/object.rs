489727be7ad6f801bc61f7198f6721690ab13f2a
Armin Ronacher <armin.ronacher@active-4.com>
2017-09-11 16:09:41 +0200

    feat(debuginfo): Updated macho abstraction

--- debuginfo/src/object.rs
+++ debuginfo/src/object.rs
@@ -8,19 +8,20 @@ use goblin;
 use symbolic_common::{ErrorKind, Result, ByteView, Arch};
 
 
-enum ObjectKind<'a> {
+enum FatObjectKind<'a> {
     Elf(goblin::elf::Elf<'a>),
     MachO(goblin::mach::Mach<'a>),
 }
 
-enum VariantTarget<'a> {
+enum ObjectTarget<'a> {
     MachOBin(&'a goblin::mach::MachO<'a>),
-    MachOFat(goblin::mach::fat::FatArch),
+    MachOFat(goblin::mach::fat::FatArch, goblin::mach::Mach<'a>),
 }
 
 pub struct Object<'a> {
+    fat_object: &'a FatObject<'a>,
     arch: Arch,
-    target: VariantTarget<'a>,
+    target: ObjectTarget<'a>,
 }
 
 impl<'a> Object<'a> {
@@ -40,7 +41,7 @@ impl<'a> Object<'a> {
 /// Represents an object file.
 pub struct FatObject<'a> {
     byteview: &'a ByteView<'a>,
-    kind: ObjectKind<'a>,
+    kind: FatObjectKind<'a>,
 }
 
 impl<'a> FatObject<'a> {
@@ -51,13 +52,13 @@ impl<'a> FatObject<'a> {
             let mut cur = Cursor::new(buf);
             match goblin::peek(&mut cur)? {
                 goblin::Hint::Elf(_) => {
-                    ObjectKind::Elf(goblin::elf::Elf::parse(buf)?)
+                    FatObjectKind::Elf(goblin::elf::Elf::parse(buf)?)
                 }
                 goblin::Hint::Mach(_) => {
-                    ObjectKind::MachO(goblin::mach::Mach::parse(buf)?)
+                    FatObjectKind::MachO(goblin::mach::Mach::parse(buf)?)
                 }
                 goblin::Hint::MachFat(_) => {
-                    ObjectKind::MachO(goblin::mach::Mach::parse(buf)?)
+                    FatObjectKind::MachO(goblin::mach::Mach::parse(buf)?)
                 }
                 _ => {
                     return Err(ErrorKind::UnsupportedObjectFile.into());
@@ -74,26 +75,31 @@ impl<'a> FatObject<'a> {
     pub fn variants(&'a self) -> Result<Vec<Object<'a>>> {
         let mut rv = vec![];
         match self.kind {
-            ObjectKind::Elf(ref elf) => {
+            FatObjectKind::Elf(ref elf) => {
                 return Err(ErrorKind::UnsupportedObjectFile.into());
             }
-            ObjectKind::MachO(ref mach) => {
+            FatObjectKind::MachO(ref mach) => {
                 match *mach {
                     goblin::mach::Mach::Fat(ref fat) => {
                         for arch in fat.iter_arches() {
                             let arch = arch?;
+                            let data = &self.byteview[arch.offset as usize..
+                                                      (arch.offset + arch.size) as usize];
+                            let m = goblin::mach::Mach::parse(data)?;
                             rv.push(Object {
+                                fat_object: self,
                                 arch: Arch::from_mach(arch.cputype as u32,
                                                       arch.cpusubtype as u32)?,
-                                target: VariantTarget::MachOFat(arch),
+                                target: ObjectTarget::MachOFat(arch, m),
                             });
                         }
                     }
                     goblin::mach::Mach::Binary(ref macho) => {
                         rv.push(Object {
+                            fat_object: self,
                             arch: Arch::from_mach(macho.header.cputype as u32,
                                                   macho.header.cpusubtype as u32)?,
-                            target: VariantTarget::MachOBin(macho),
+                            target: ObjectTarget::MachOBin(macho),
                         });
                     }
                 }
