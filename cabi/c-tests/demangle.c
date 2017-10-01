52bfff47f900834677986cb660d393e27614ab99
Armin Ronacher <armin.ronacher@active-4.com>
2017-10-01 20:47:24 +0200

    feat(cabi): Added a basic example for how to use the C-ABI

---
+++ cabi/c-tests/demangle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <assert.h>
+#include "symbolic.h"
+
+
+int main()
+{
+    const char *mangled = "__ZN9backtrace5dylib5Dylib3get28_$u7b$$u7b$closure$u7d$$u7d$17hc7d4a2b070814ae3E";
+    char *demangled = symbolic_demangle(mangled);
+    printf("Mangled: %s\n", mangled);
+    printf("Demangled: %s\n", demangled);
+    symbolic_cstr_free(demangled);
+
+    symbolic_err_clear();
+    char *rv = symbolic_demangle("\xff\x23");
+    assert(rv == 0);
+    printf("Error code: %d\n", symbolic_err_get_last_code());
+    char *msg = symbolic_err_get_last_message();
+    printf("Error message: %s\n", msg);
+    symbolic_cstr_free(msg);
+
+    return 0;
+}
