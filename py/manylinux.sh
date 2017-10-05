fb20611285c649a1c8c7d376de40c6bffb608975
Jan Michael Auer <jan.auer@sentry.io>
2017-10-05 13:59:50 +0200

    feat(wheels): Run auditwheels to generate manylinux1 wheels

--- py/manylinux.sh
+++ py/manylinux.sh
@@ -11,3 +11,9 @@ export PATH=~/.cargo/bin:$PATH
 # Build wheels
 which linux32 && LINUX32=linux32
 $LINUX32 /opt/python/cp27-cp27mu/bin/python setup.py bdist_wheel
+
+# Audit wheels
+for wheel in dist/*-linux_*.whl; do
+  auditwheel repair $wheel -w dist/
+  rm $wheel
+done
