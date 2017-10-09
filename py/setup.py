b7694eb8f65e613c84bc716fc5c98c3d4155c3dd
Armin Ronacher <armin.ronacher@active-4.com>
2017-10-09 13:31:21 +0200

    build(py): Added a way to control debug vs release builds

--- py/setup.py
+++ py/setup.py
@@ -1,3 +1,4 @@
+import os
 import re
 from setuptools import setup, find_packages
 
@@ -5,20 +6,25 @@ from setuptools import setup, find_packages
 _version_re = re.compile(r'^version\s*=\s*"(.*?)"\s*$(?m)')
 
 
+DEBUG_BUILD = os.environ.get('SYMBOLIC_DEBUG') == '1'
+
+
 with open('../Cargo.toml') as f:
     version = _version_re.search(f.read()).group(1)
 
 
 def build_native(spec):
+    target = DEBUG_BUILD and 'debug' or 'release'
+
     # Step 1: build the rust library
     build = spec.add_external_build(
-        cmd=['cargo', 'build', '--release'],
+        cmd=['cargo', 'build', '--' + target],
         path='../cabi'
     )
 
     spec.add_cffi_module(
         module_path='symbolic._lowlevel',
-        dylib=lambda: build.find_dylib('symbolic', in_path='target/release'),
+        dylib=lambda: build.find_dylib('symbolic', in_path='target/%s' % target),
         header_filename=lambda: build.find_header('symbolic.h', in_path='include'),
         rtld_flags=['NOW', 'NODELETE']
     )
