dd375abccfc84afc0a95b068fe236e2bb52bdedc
Armin Ronacher <armin.ronacher@active-4.com>
2017-10-24 10:49:20 +0200

    py: Added sdist support

--- py/setup.py
+++ py/setup.py
@@ -1,6 +1,12 @@
 import os
 import re
+import atexit
+import shutil
+import zipfile
+import tempfile
+import subprocess
 from setuptools import setup, find_packages
+from distutils.command.sdist import sdist
 
 
 _version_re = re.compile(r'^version\s*=\s*"(.*?)"\s*$(?m)')
@@ -8,9 +14,33 @@ _version_re = re.compile(r'^version\s*=\s*"(.*?)"\s*$(?m)')
 
 DEBUG_BUILD = os.environ.get('SYMBOLIC_DEBUG') == '1'
 
+with open('README', 'rb') as f:
+    readme = f.read()
 
-with open('../Cargo.toml') as f:
-    version = _version_re.search(f.read()).group(1)
+
+if os.path.isfile('../Cargo.toml'):
+    with open('../Cargo.toml') as f:
+        version = _version_re.search(f.read()).group(1)
+else:
+    with open('version.txt') as f:
+        version = f.readline().strip()
+
+
+def vendor_rust_deps():
+    subprocess.Popen(['git', 'archive', '--worktree-attributes',
+                      '-o', 'py/rustsrc.zip', 'HEAD'], cwd='..').wait()
+
+
+def write_version():
+    with open('version.txt', 'wb') as f:
+        f.write('%s\n' % version)
+
+
+class CustomSDist(sdist):
+    def run(self):
+        vendor_rust_deps()
+        write_version()
+        sdist.run(self)
 
 
 def build_native(spec):
@@ -21,10 +51,23 @@ def build_native(spec):
     else:
         target = 'debug'
 
+    # Step 0: find rust sources
+    if os.path.isfile('rustsrc.zip'):
+        scratchpad = tempfile.mkdtemp()
+        @atexit.register
+        def delete_scratchpad():
+            shutil.rmtree(scratchpad)
+        zf = zipfile.ZipFile('rustsrc.zip')
+        zf.extractall(scratchpad)
+        rust_path = scratchpad + '/cabi'
+    else:
+        rust_path = '../cabi'
+        scratchpad = None
+
     # Step 1: build the rust library
     build = spec.add_external_build(
         cmd=cmd,
-        path='../cabi'
+        path=rust_path
     )
 
     spec.add_cffi_module(
@@ -39,6 +82,11 @@ setup(
     name='symbolic',
     version=version,
     packages=find_packages(),
+    author='Sentry',
+    license='MIT',
+    author_email='hello@sentry.io',
+    description='A python library for dealing with symbol files and more.',
+    long_description=readme,
     include_package_data=True,
     zip_safe=False,
     platforms='any',
@@ -50,5 +98,8 @@ setup(
     ],
     milksnake_tasks=[
         build_native,
-    ]
+    ],
+    cmdclass={
+        'sdist': CustomSDist,
+    }
 )
