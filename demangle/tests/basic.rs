6594a5decfaf20b525ac06720169f0fcc832e372
Jan Michael Auer <jan.auer@sentry.io>
2017-12-21 13:57:05 +0100

    test(demangle): Add more tests for language detection

--- demangle/tests/basic.rs
+++ demangle/tests/basic.rs
@@ -28,18 +28,6 @@ fn test_rust_demangle() {
 }
 
 #[test]
-fn test_objcpp_cpp_demangle() {
-    let name = Name::with_language("_ZN4base24MessagePumpNSApplication5DoRunEPNS_11MessagePump8DelegateE", Language::ObjCpp);
-    assert_eq!(&name.try_demangle(Default::default()), "base::MessagePumpNSApplication::DoRun");
-}
-
-#[test]
-fn test_objcpp_objc_demangle() {
-    let name = Name::with_language("+[KSCrashReportFilterObjectForKey filterWithKey:allowNotFound:]", Language::ObjCpp);
-    assert_eq!(&name.try_demangle(Default::default()), "+[KSCrashReportFilterObjectForKey filterWithKey:allowNotFound:]");
-}
-
-#[test]
 fn test_cpp_demangle() {
     assert_mangle("_Z28JS_GetPropertyDescriptorByIdP9JSContextN2JS6HandleIP8JSObjectEENS2_I4jsidEENS1_13MutableHandleINS1_18PropertyDescriptorEEE",
                   Some("JS_GetPropertyDescriptorById"), DemangleOptions {
@@ -50,13 +38,6 @@ fn test_cpp_demangle() {
     let name = Name::new("_Z28JS_GetPropertyDescriptorByIdP9JSContextN2JS6HandleIP8JSObjectEENS2_I4jsidEENS1_13MutableHandleINS1_18PropertyDescriptorEEE");
     assert_eq!(name.detect_language(), Some(Language::Cpp));
     assert_eq!(&name.try_demangle(Default::default()), "JS_GetPropertyDescriptorById");
-}
-
-#[test]
-fn test_cpp_potential_rust_demangle() {
-    // TODO: This namebol yields inconsistent results in C++
-    let name = Name::new("_ZN4base8internal7InvokerINS0_9BindStateIMN4mate19TrackableObjectBaseEFvvEJNS_7WeakPtrIS4_EEEEEFvvEE7RunImplIRKS6_RKNSt3__15tupleIJS8_EEEJLm0EEEEvOT_OT0_NS_13IndexSequenceIJXspT1_EEEE");
-    assert_eq!(name.detect_language(), Some(Language::Cpp));
 }
 
 #[test]
---
+++ demangle/tests/detection.rs
@@ -0,0 +1,72 @@
+//! Language auto-detection tests
+
+extern crate symbolic_common;
+extern crate symbolic_demangle;
+
+use symbolic_common::{Language, Name};
+use symbolic_demangle::Demangle;
+
+fn assert_language(input: &str, lang: Language) {
+    let name = Name::new(input);
+    assert_eq!(name.detect_language(), Some(lang));
+}
+
+fn assert_none(input: &str) {
+    let name = Name::new(input);
+    assert_eq!(name.detect_language(), None);
+}
+
+#[test]
+fn unknown() {
+    // Fallback to test false positives
+    assert_none("xxxxxxxxxxx");
+}
+
+#[test]
+fn basic_cpp() {
+    // This is a symbol generated for "void h(int, char)" by:
+    //  - Intel C++ 8.0 for Linux
+    //  - HP aC++ A.05.55 IA-64
+    //  - IAR EWARM C++ 5.4 ARM
+    //  - GCC 3.x and higher
+    //  - Clang 1.x and higher[1]
+    //
+    // NOTE: Microsoft Visual C++ would generate a different
+    // symbol, but we do not support that yet.
+    assert_language("_Z1hic", Language::Cpp);
+}
+
+#[test]
+fn basic_rust() {
+    assert_language(
+        "__ZN3std2io4Read11read_to_end17hb85a0f6802e14499E",
+        Language::Rust,
+    );
+}
+
+#[test]
+fn basic_objc_static() {
+    assert_language("+[Foo bar:blub:]", Language::ObjC);
+}
+
+#[test]
+fn basic_objc_member() {
+    assert_language("-[Foo bar:blub:]", Language::ObjC);
+}
+
+#[test]
+fn basic_swift() {
+    assert_language("_T08mangling3barSiyKF", Language::Swift);
+}
+
+#[test]
+fn ambiguous_cpp_rust() {
+    // This symbol might look like a Rust symbol at first because of the _ZN...E
+    // schema, but comes from a C++ file and is not even a valid Rust.
+    // It demangles to:
+    //     content::ContentMain(content::ContentMainParams const&)
+    assert_language(
+        "_ZN7content11ContentMainERKNS_17ContentMainParamsE",
+        Language::Cpp,
+    );
+}
