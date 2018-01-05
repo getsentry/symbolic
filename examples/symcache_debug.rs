f991f5a6a4f53be486aaefc81fc7e3f49f3b1f1c
Jan Michael Auer <jan.auer@sentry.io>
2018-01-05 15:38:55 +0100

    ref: Move examples to top level

--- examples/symcache_debug.rs
+++ examples/symcache_debug.rs
@@ -1,15 +1,14 @@
 extern crate clap;
-extern crate symbolic_symcache;
-extern crate symbolic_debuginfo;
 extern crate symbolic_common;
+extern crate symbolic_debuginfo;
+extern crate symbolic_symcache;
 
 use std::u64;
-use std::process;
 use std::fs;
 use std::io;
 use std::error::Error;
 
-use clap::{App, ArgMatches, Arg};
+use clap::{App, Arg, ArgMatches};
 
 use symbolic_symcache::SymCache;
 use symbolic_debuginfo::FatObject;
@@ -35,7 +34,11 @@ fn execute(matches: &ArgMatches) -> Result<(), Box<Error>> {
         if arch == Arch::Unknown && objects.len() != 1 {
             println!("Contained architectures:");
             for obj in objects {
-                println!("  {} [{}]", obj.uuid().unwrap_or(Default::default()), obj.arch());
+                println!(
+                    "  {} [{}]",
+                    obj.uuid().unwrap_or(Default::default()),
+                    obj.arch()
+                );
             }
             return Ok(());
         }
@@ -55,14 +58,15 @@ fn execute(matches: &ArgMatches) -> Result<(), Box<Error>> {
 
         let obj = match obj {
             Some(obj) => obj,
-            None => { return Err(err(&format!("did not find architecture {}", arch))); }
+            None => return Err(err(&format!("did not find architecture {}", arch))),
         };
 
         symcache = SymCache::from_object(obj)?;
 
         // write mode
         if matches.is_present("write_cache_file") {
-            let filename = matches.value_of("symcache_file_path")
+            let filename = matches
+                .value_of("symcache_file_path")
                 .map(|x| x.to_string())
                 .unwrap_or_else(|| format!("{}.symcache", file_path));
             symcache.to_writer(fs::File::create(&filename)?)?;
@@ -113,37 +117,53 @@ fn execute(matches: &ArgMatches) -> Result<(), Box<Error>> {
 fn main() {
     let matches = App::new("symcache-debug")
         .about("Works with symbol files with the symcache interface")
-        .arg(Arg::with_name("debug_file_path")
-             .short("d")
-             .long("debug-file")
-             .value_name("PATH")
-             .help("Path to the debug info file"))
-        .arg(Arg::with_name("write_cache_file")
-             .short("w")
-             .long("write-cache-file")
-             .help("Write the cache file from the debug info file.  If no file name is \
-                    provided via --symcache-file it will be written to the source file \
-                    with the .symcache suffix."))
-        .arg(Arg::with_name("arch")
-             .short("a")
-             .long("arch")
-             .value_name("ARCH")
-             .help("The architecture of the object to work with."))
-        .arg(Arg::with_name("report")
-             .long("report")
-             .help("Spit out some debug information"))
-        .arg(Arg::with_name("symcache_file_path")
-             .short("c")
-             .long("symcache-file")
-             .value_name("PATH")
-             .help("Path to the symcache file"))
-        .arg(Arg::with_name("lookup_addr")
-             .long("lookup")
-             .value_name("ADDR")
-             .help("Looks up an address in the debug file"))
-        .arg(Arg::with_name("print_symbols")
-             .long("symbols")
-             .help("Print all symbols"))
+        .arg(
+            Arg::with_name("debug_file_path")
+                .short("d")
+                .long("debug-file")
+                .value_name("PATH")
+                .help("Path to the debug info file"),
+        )
+        .arg(
+            Arg::with_name("write_cache_file")
+                .short("w")
+                .long("write-cache-file")
+                .help(
+                    "Write the cache file from the debug info file.  If no file name is \
+                     provided via --symcache-file it will be written to the source file \
+                     with the .symcache suffix.",
+                ),
+        )
+        .arg(
+            Arg::with_name("arch")
+                .short("a")
+                .long("arch")
+                .value_name("ARCH")
+                .help("The architecture of the object to work with."),
+        )
+        .arg(
+            Arg::with_name("report")
+                .long("report")
+                .help("Spit out some debug information"),
+        )
+        .arg(
+            Arg::with_name("symcache_file_path")
+                .short("c")
+                .long("symcache-file")
+                .value_name("PATH")
+                .help("Path to the symcache file"),
+        )
+        .arg(
+            Arg::with_name("lookup_addr")
+                .long("lookup")
+                .value_name("ADDR")
+                .help("Looks up an address in the debug file"),
+        )
+        .arg(
+            Arg::with_name("print_symbols")
+                .long("symbols")
+                .help("Print all symbols"),
+        )
         .get_matches();
 
     execute(&matches).unwrap()
