85f254fdd6e4169ab9db90f87d99edd6b69d69c8
Jan Michael Auer <jan.auer@sentry.io>
2018-01-31 12:58:39 +0100

    feat(debuginfo): Introduce a new ObjectId type

--- debuginfo/src/object.rs
+++ debuginfo/src/object.rs
@@ -12,6 +12,60 @@ use dwarf::DwarfData;
 use elf::{get_elf_uuid, get_elf_vmaddr};
 use mach::{get_mach_uuid, get_mach_vmaddr};
 
+/// Unique identifier for `Object` files and their debug information.
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Clone, Copy)]
+pub struct ObjectId {
+    uuid: Uuid,
+    age: u32,
+}
+
+impl ObjectId {
+    /// Parses a `ObjectId` from a formatted `String`.
+    ///
+    /// The string must be between 33 and 40 characters long and consist of:
+    /// 1. A 32 character uppercase hex representation of the UUID field
+    /// 2. A 1-8 character lowercase hex representation of the u32 age field
+    pub fn parse(input: &str) -> Result<ObjectId> {
+        if input.len() < 33 || input.len() > 40 {
+            return Err(ErrorKind::Parse("Invalid input string length").into());
+        }
+
+        let uuid = Uuid::parse_str(&input[..32]).map_err(|_| ErrorKind::Parse("UUID parse error"))?;
+        let age = u32::from_str_radix(&input[32..], 16)?;
+        Ok(ObjectId { uuid, age })
+    }
+
+    /// Constructs a `ObjectId` from its `uuid`.
+    pub fn from_uuid(uuid: Uuid) -> ObjectId {
+        Self::from_parts(uuid, 0)
+    }
+
+    /// Constructs a `ObjectId` from its `uuid` and `age` parts.
+    pub fn from_parts(uuid: Uuid, age: u32) -> ObjectId {
+        ObjectId { uuid, age }
+    }
+
+    /// Returns the UUID part of the code module's debug_identifier.
+    pub fn uuid(&self) -> Uuid {
+        self.uuid
+    }
+
+    /// Returns the age part of the code module's debug identifier.
+    ///
+    /// On Windows, this is an incrementing counter to identify the build.
+    /// On all other platforms, this value will always be zero.
+    pub fn age(&self) -> u32 {
+        self.age
+    }
+}
+
+impl fmt::Display for ObjectId {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let uuid = self.uuid.simple().to_string().to_uppercase();
+        write!(f, "{}{:x}", uuid, self.age)
+    }
+}
+
 /// Contains type specific data of `Object`s
 pub(crate) enum ObjectTarget<'bytes> {
     Breakpad(&'bytes BreakpadSym),
@@ -285,5 +339,62 @@ impl<'bytes> FatObject<'bytes> {
     /// Returns a iterator over object variants in this fat object.
     pub fn objects(&'bytes self) -> Objects<'bytes> {
         Objects::new(self)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse_short() {
+        assert_eq!(
+            ObjectId::parse("DFB8E43AF2423D73A453AEB6A777EF75a").unwrap(),
+            ObjectId {
+                uuid: Uuid::parse_str("DFB8E43AF2423D73A453AEB6A777EF75").unwrap(),
+                age: 10,
+            }
+        );
+    }
+
+    #[test]
+    fn test_parse_long() {
+        assert_eq!(
+            ObjectId::parse("DFB8E43AF2423D73A453AEB6A777EF75feedface").unwrap(),
+            ObjectId {
+                uuid: Uuid::parse_str("DFB8E43AF2423D73A453AEB6A777EF75").unwrap(),
+                age: 4277009102,
+            }
+        );
+    }
+
+    #[test]
+    fn test_to_string_short() {
+        let id = ObjectId {
+            uuid: Uuid::parse_str("DFB8E43AF2423D73A453AEB6A777EF75").unwrap(),
+            age: 10,
+        };
+
+        assert_eq!(id.to_string(), "DFB8E43AF2423D73A453AEB6A777EF75a");
+    }
+
+    #[test]
+    fn test_to_string_long() {
+        let id = ObjectId {
+            uuid: Uuid::parse_str("DFB8E43AF2423D73A453AEB6A777EF75").unwrap(),
+            age: 4277009102,
+        };
+
+        assert_eq!(id.to_string(), "DFB8E43AF2423D73A453AEB6A777EF75feedface");
+    }
+
+    #[test]
+    fn test_parse_error_short() {
+        assert!(ObjectId::parse("DFB8E43AF2423D73A453AEB6A777EF75").is_err());
+    }
+
+    #[test]
+    fn test_parse_error_long() {
+        assert!(ObjectId::parse("DFB8E43AF2423D73A453AEB6A777EF75feedface1").is_err())
     }
 }
