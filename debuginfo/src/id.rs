ce3328f1e1956ccfef80548d8382fe21ca8cf4d9
Jan Michael Auer <jan.auer@sentry.io>
2018-03-14 16:40:16 +0100

    ref(debuginfo): Implement the new debug identifier format

--- debuginfo/src/id.rs
+++ debuginfo/src/id.rs
@@ -1,38 +1,49 @@
 use std::fmt;
+use std::str;
 use uuid::Uuid;
-use symbolic_common::{ErrorKind, Result};
+use symbolic_common::{Error, ErrorKind, Result};
 
 /// Unique identifier for `Object` files and their debug information.
+///
+/// The string representation must be between 33 and 40 characters long and
+/// consist of:
+///
+/// 1. 36 character hyphenated hex representation of the UUID field
+/// 2. 1-16 character lowercase hex representation of the u64 appendix
+///
+/// **Example:**
+///
+/// ```
+/// # extern crate symbolic_common;
+/// # extern crate symbolic_debuginfo;
+/// use std::str::FromStr;
+/// # use symbolic_common::Result;
+/// use symbolic_debuginfo::ObjectId;
+///
+/// # fn foo() -> Result<()> {
+/// let id = ObjectId::from_str("dfb8e43a-f242-3d73-a453-aeb6a777ef75-a")?;
+/// assert_eq!("dfb8e43a-f242-3d73-a453-aeb6a777ef75-a".to_string(), id.to_string());
+/// # Ok(())
+/// # }
+///
+/// # fn main() { foo().unwrap() }
+/// ```
+#[repr(C, packed)]
 #[derive(Debug, Default, Eq, PartialEq, Ord, PartialOrd, Hash, Clone, Copy)]
 pub struct ObjectId {
     uuid: Uuid,
-    age: u32,
+    appendix: u64,
 }
 
 impl ObjectId {
-    /// Parses an `ObjectId` from a formatted `String`.
-    ///
-    /// The string must be between 33 and 40 characters long and consist of:
-    /// 1. A 32 character uppercase hex representation of the UUID field
-    /// 2. A 1-8 character lowercase hex representation of the u32 age field
-    pub fn parse(input: &str) -> Result<ObjectId> {
-        if input.len() < 33 || input.len() > 40 {
-            return Err(ErrorKind::Parse("Invalid input string length").into());
-        }
-
-        let uuid = Uuid::parse_str(&input[..32]).map_err(|_| ErrorKind::Parse("UUID parse error"))?;
-        let age = u32::from_str_radix(&input[32..], 16)?;
-        Ok(ObjectId { uuid, age })
-    }
-
     /// Constructs a `ObjectId` from its `uuid`.
     pub fn from_uuid(uuid: Uuid) -> ObjectId {
         Self::from_parts(uuid, 0)
     }
 
-    /// Constructs a `ObjectId` from its `uuid` and `age` parts.
-    pub fn from_parts(uuid: Uuid, age: u32) -> ObjectId {
-        ObjectId { uuid, age }
+    /// Constructs a `ObjectId` from its `uuid` and `appendix` parts.
+    pub fn from_parts(uuid: Uuid, appendix: u64) -> ObjectId {
+        ObjectId { uuid, appendix }
     }
 
     /// Returns the UUID part of the code module's debug_identifier.
@@ -40,19 +51,39 @@ impl ObjectId {
         self.uuid
     }
 
-    /// Returns the age part of the code module's debug identifier.
+    /// Returns the appendix part of the code module's debug identifier.
     ///
     /// On Windows, this is an incrementing counter to identify the build.
     /// On all other platforms, this value will always be zero.
-    pub fn age(&self) -> u32 {
-        self.age
+    pub fn appendix(&self) -> u64 {
+        self.appendix
     }
 }
 
 impl fmt::Display for ObjectId {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let uuid = self.uuid.simple().to_string().to_uppercase();
-        write!(f, "{}{:x}", uuid, self.age)
+        self.uuid.fmt(f)?;
+        if self.appendix > 0 {
+            write!(f, "-{:x}", { self.appendix })?;
+        }
+        Ok(())
+    }
+}
+
+impl str::FromStr for ObjectId {
+    type Err = Error;
+
+    fn from_str(string: &str) -> Result<ObjectId> {
+        if string.len() < 36 || string.len() > 53 {
+            return Err(ErrorKind::Parse("Invalid input string length").into());
+        }
+
+        let uuid =
+            Uuid::parse_str(&string[..36]).map_err(|_| ErrorKind::Parse("UUID parse error"))?;
+        let appendix = string
+            .get(37..)
+            .map_or(Ok(0), |s| u64::from_str_radix(s, 16))?;
+        Ok(ObjectId { uuid, appendix })
     }
 }
 
@@ -62,17 +93,36 @@ impl From<Uuid> for ObjectId {
     }
 }
 
+impl From<(Uuid, u64)> for ObjectId {
+    fn from(tuple: (Uuid, u64)) -> ObjectId {
+        let (uuid, appendix) = tuple;
+        ObjectId::from_parts(uuid, appendix)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_parse_zero() {
+        assert_eq!(
+            ObjectId::from_str("dfb8e43a-f242-3d73-a453-aeb6a777ef75").unwrap(),
+            ObjectId {
+                uuid: Uuid::parse_str("dfb8e43a-f242-3d73-a453-aeb6a777ef75").unwrap(),
+                appendix: 0,
+            }
+        );
+    }
 
     #[test]
     fn test_parse_short() {
         assert_eq!(
-            ObjectId::parse("DFB8E43AF2423D73A453AEB6A777EF75a").unwrap(),
+            ObjectId::from_str("dfb8e43a-f242-3d73-a453-aeb6a777ef75-a").unwrap(),
             ObjectId {
-                uuid: Uuid::parse_str("DFB8E43AF2423D73A453AEB6A777EF75").unwrap(),
-                age: 10,
+                uuid: Uuid::parse_str("dfb8e43a-f242-3d73-a453-aeb6a777ef75").unwrap(),
+                appendix: 10,
             }
         );
     }
@@ -80,41 +130,56 @@ mod test {
     #[test]
     fn test_parse_long() {
         assert_eq!(
-            ObjectId::parse("DFB8E43AF2423D73A453AEB6A777EF75feedface").unwrap(),
+            ObjectId::from_str("dfb8e43a-f242-3d73-a453-aeb6a777ef75-feedface").unwrap(),
             ObjectId {
-                uuid: Uuid::parse_str("DFB8E43AF2423D73A453AEB6A777EF75").unwrap(),
-                age: 4277009102,
+                uuid: Uuid::parse_str("dfb8e43a-f242-3d73-a453-aeb6a777ef75").unwrap(),
+                appendix: 4277009102,
             }
         );
     }
 
     #[test]
-    fn test_to_string_short() {
+    fn test_to_string_zero() {
         let id = ObjectId {
-            uuid: Uuid::parse_str("DFB8E43AF2423D73A453AEB6A777EF75").unwrap(),
-            age: 10,
+            uuid: Uuid::parse_str("dfb8e43a-f242-3d73-a453-aeb6a777ef75").unwrap(),
+            appendix: 0,
         };
 
-        assert_eq!(id.to_string(), "DFB8E43AF2423D73A453AEB6A777EF75a");
+        assert_eq!(id.to_string(), "dfb8e43a-f242-3d73-a453-aeb6a777ef75");
+    }
+
+    #[test]
+    fn test_to_string_short() {
+        let id = ObjectId {
+            uuid: Uuid::parse_str("dfb8e43a-f242-3d73-a453-aeb6a777ef75").unwrap(),
+            appendix: 10,
+        };
+
+        assert_eq!(id.to_string(), "dfb8e43a-f242-3d73-a453-aeb6a777ef75-a");
     }
 
     #[test]
     fn test_to_string_long() {
         let id = ObjectId {
-            uuid: Uuid::parse_str("DFB8E43AF2423D73A453AEB6A777EF75").unwrap(),
-            age: 4277009102,
+            uuid: Uuid::parse_str("dfb8e43a-f242-3d73-a453-aeb6a777ef75").unwrap(),
+            appendix: 4277009102,
         };
 
-        assert_eq!(id.to_string(), "DFB8E43AF2423D73A453AEB6A777EF75feedface");
+        assert_eq!(
+            id.to_string(),
+            "dfb8e43a-f242-3d73-a453-aeb6a777ef75-feedface"
+        );
     }
 
     #[test]
     fn test_parse_error_short() {
-        assert!(ObjectId::parse("DFB8E43AF2423D73A453AEB6A777EF75").is_err());
+        assert!(ObjectId::from_str("dfb8e43a-f242-3d73-a453-aeb6a777ef7").is_err());
     }
 
     #[test]
     fn test_parse_error_long() {
-        assert!(ObjectId::parse("DFB8E43AF2423D73A453AEB6A777EF75feedface1").is_err())
+        assert!(
+            ObjectId::from_str("dfb8e43a-f242-3d73-a453-aeb6a777ef75-feedfacefeedface1").is_err()
+        )
     }
 }
