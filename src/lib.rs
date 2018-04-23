5ad8ce4d90db28d0077214b0e317f365e3826c79
Jan Michael Auer <jan.auer@sentry.io>
2018-04-23 15:48:56 +0200

    feat: Create features for every top-level crate

--- src/lib.rs
+++ src/lib.rs
@@ -7,22 +7,34 @@
 #[doc(hidden)]
 pub extern crate symbolic_common;
 #[doc(hidden)]
+#[cfg(feature = "debuginfo")]
 pub extern crate symbolic_debuginfo;
 #[doc(hidden)]
+#[cfg(feature = "demangle")]
 pub extern crate symbolic_demangle;
 #[doc(hidden)]
+#[cfg(feature = "minidump")]
 pub extern crate symbolic_minidump;
 #[doc(hidden)]
+#[cfg(feature = "proguard")]
 pub extern crate symbolic_proguard;
 #[doc(hidden)]
+#[cfg(feature = "sourcemap")]
 pub extern crate symbolic_sourcemap;
 #[doc(hidden)]
+#[cfg(feature = "symcache")]
 pub extern crate symbolic_symcache;
 
-pub use symbolic_proguard as proguard;
-pub use symbolic_sourcemap as sourcemap;
-pub use symbolic_demangle as demangle;
-pub use symbolic_minidump as minidump;
-pub use symbolic_symcache as symcache;
-pub use symbolic_debuginfo as debuginfo;
 pub use symbolic_common as common;
+#[cfg(feature = "debuginfo")]
+pub use symbolic_debuginfo as debuginfo;
+#[cfg(feature = "demangle")]
+pub use symbolic_demangle as demangle;
+#[cfg(feature = "minidump")]
+pub use symbolic_minidump as minidump;
+#[cfg(feature = "proguard")]
+pub use symbolic_proguard as proguard;
+#[cfg(feature = "sourcemap")]
+pub use symbolic_sourcemap as sourcemap;
+#[cfg(feature = "symcache")]
+pub use symbolic_symcache as symcache;
