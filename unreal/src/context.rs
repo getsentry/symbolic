ccd9bb0372dcf62c8b451cf86a8041db0a2507e2
Bruno Garcia <bruno@brunogarcia.com>
2018-12-07 16:04:03 +0100

    test: verify prop access

--- unreal/src/context.rs
+++ unreal/src/context.rs
@@ -27,9 +27,9 @@ pub struct Unreal4ContextRuntimeProperties {
     pub is_internal_build: Option<bool>,
     /// IsSourceDistribution
     pub is_source_distribution: Option<bool>,
-    /// IsEnsure
-    pub is_assert: Option<bool>,
     /// IsAssert
+    pub is_assert: Option<bool>,
+    /// IsEnsure
     pub is_ensure: Option<bool>,
     /// CrashType
     pub crash_type: Option<String>,
@@ -145,16 +145,79 @@ fn get_runtime_properties(root: &Element) -> Option<Unreal4ContextRuntimePropert
 
     let mut rv = Unreal4ContextRuntimeProperties::default();
 
+    fn get_text_or_none(elm: &Element) -> Option<String> {
+        let text = elm.text();
+        if text != "" {
+            Some(text.to_string())
+        } else {
+            None
+        }
+    }
+
     for child in list.children() {
-        if child.tag() == &QName::from("CrashGUID") {
-            let text = child.text();
-            if text != "" {
-                rv.crash_guid = Some(child.text().to_string());
+        let tag = child.tag();
+
+        // We don't expect an XML with namespace here
+        if !tag.ns().is_none() {
+            continue;
+        }
+        match tag.name() {
+            "CrashGUID" => rv.crash_guid = get_text_or_none(&child),
+            "ProcessId" => rv.process_id = child.text().parse::<u32>().ok(),
+            "IsInternalBuild" => rv.is_internal_build = child.text().parse::<bool>().ok(),
+            "IsSourceDistribution" => rv.is_source_distribution = child.text().parse::<bool>().ok(),
+            "IsAssert" => rv.is_assert = child.text().parse::<bool>().ok(),
+            "IsEnsure" => rv.is_ensure = child.text().parse::<bool>().ok(),
+            "CrashType" => rv.crash_type = get_text_or_none(&child),
+            "SecondsSinceStart" => rv.seconds_since_start = child.text().parse::<u32>().ok(),
+            "GameName" => rv.game_name = get_text_or_none(&child),
+            "ExecutableName" => rv.executable_name = get_text_or_none(&child),
+            "BuildConfiguration" => rv.build_configuration = get_text_or_none(&child),
+            "PlatformName" => rv.platform_name = get_text_or_none(&child),
+            "EngineMode" => rv.engine_mode = get_text_or_none(&child),
+            "EngineVersion" => rv.engine_version = get_text_or_none(&child),
+            "LanguageLCID" => rv.language_lcid = child.text().parse::<i32>().ok(),
+            "AppDefaultLocale" => rv.app_default_locate = get_text_or_none(&child),
+            "BuildVersion" => rv.build_version = get_text_or_none(&child),
+            "IsUE4Release" => rv.is_ue4_release = child.text().parse::<bool>().ok(),
+            "UserName" => rv.username = get_text_or_none(&child),
+            "BaseDir" => rv.base_dir = get_text_or_none(&child),
+            "RootDir" => rv.root_dir = get_text_or_none(&child),
+            "MachineId" => rv.machine_id = get_text_or_none(&child),
+            "LoginId" => rv.login_id = get_text_or_none(&child),
+            "EpicAccountId" => rv.epic_account_id = get_text_or_none(&child),
+            "CallStack" => rv.legacy_call_stack = get_text_or_none(&child),
+            "PCallStack" => rv.portable_call_stack = get_text_or_none(&child),
+            "ErrorMessage" => rv.error_message = get_text_or_none(&child),
+            "CrashReporterMessage" => rv.crash_reporter_message = get_text_or_none(&child),
+            "Misc.NumberOfCores" => rv.misc_number_of_cores = child.text().parse::<u32>().ok(),
+            "Misc.NumberOfCoresIncludingHyperthreads" => {
+                rv.misc_number_of_cores_inc_hyperthread = child.text().parse::<u32>().ok()
             }
-        } else if child.tag() == &QName::from("ProcessId") {
-            rv.process_id = child.text().parse::<u32>().ok();
-        } else if child.tag() == &QName::from("IsInternalBuild") {
-            rv.is_internal_build = child.text().parse::<bool>().ok();
+            "Misc.Is64bitOperatingSystem" => rv.misc_is_64bit = child.text().parse::<bool>().ok(),
+            "Misc.CPUVendor" => rv.misc_cpu_vendor = get_text_or_none(&child),
+            "Misc.CPUBrand" => rv.misc_cpu_brand = get_text_or_none(&child),
+            "Misc.PrimaryGPUBrand" => rv.misc_primary_cpu_brand = get_text_or_none(&child),
+            "Misc.OSVersionMajor" => rv.misc_os_version_major = get_text_or_none(&child),
+            "Misc.OSVersionMinor" => rv.misc_os_version_minor = get_text_or_none(&child),
+            "GameStateName" => rv.game_state_name = get_text_or_none(&child),
+            "MemoryStats.TotalPhysical" => {
+                rv.memory_stats_total_physical = child.text().parse::<u64>().ok()
+            }
+            "MemoryStats.TotalVirtual" => {
+                rv.memory_stats_total_virtual = child.text().parse::<u64>().ok()
+            }
+            "MemoryStats.PageSize" => rv.memory_stats_page_size = child.text().parse::<u64>().ok(),
+            "MemoryStats.TotalPhysicalGB" => {
+                rv.memory_stats_total_phsysical_gb = child.text().parse::<u32>().ok()
+            }
+            "TimeOfCrash" => rv.time_of_crash = child.text().parse::<u64>().ok(),
+            "bAllowToBeContacted" => rv.allowed_to_be_contacted = child.text().parse::<bool>().ok(),
+            "CrashReportClientVersion" => {
+                rv.crash_reporter_client_version = get_text_or_none(&child)
+            }
+            "Modules" => rv.modules = get_text_or_none(&child),
+            _ => {}
         }
     }
 
@@ -224,7 +287,7 @@ fn test_get_platform_properties_no_children() {
     assert_eq!(Unreal4ContextPlatformProperties::default(), actual)
 }
 
-macro_rules! test_unreal_context_str {
+macro_rules! test_unreal_runtime_properties {
     ($name:ident, $xml_elm:expr, $expect:expr $(,)*) => {
         mod $name {
             use super::*;
@@ -253,14 +316,108 @@ macro_rules! test_unreal_context_str {
     };
 }
 
-test_unreal_context_str!(
+test_unreal_runtime_properties!(
     crash_guid,
     "CrashGUID",
     "UE4CC-Windows-379993BB42BD8FBED67986857D8844B5_0000",
 );
 
-test_unreal_context_str!(process_id, "ProcessId", 2576);
-test_unreal_context_str!(is_internal_build, "IsInternalBuild", true);
+test_unreal_runtime_properties!(process_id, "ProcessId", 2576);
+test_unreal_runtime_properties!(is_internal_build, "IsInternalBuild", true);
+test_unreal_runtime_properties!(is_source_distribution, "IsSourceDistribution", true);
+test_unreal_runtime_properties!(is_ensure, "IsEnsure", true);
+test_unreal_runtime_properties!(is_assert, "IsAssert", true);
+test_unreal_runtime_properties!(crash_type, "CrashType", "Crash");
+test_unreal_runtime_properties!(seconds_since_start, "SecondsSinceStart", 4);
+test_unreal_runtime_properties!(game_name, "GameName", "UE4-YetAnother");
+test_unreal_runtime_properties!(executable_name, "ExecutableName", "YetAnother");
+test_unreal_runtime_properties!(build_configuration, "BuildConfiguration", "Development");
+test_unreal_runtime_properties!(platform_name, "PlatformName", "WindowsNoEditor");
+test_unreal_runtime_properties!(engine_mode, "EngineMode", "Game");
+test_unreal_runtime_properties!(engine_version, "EngineVersion", "Game");
+test_unreal_runtime_properties!(language_lcid, "LanguageLCID", 1033);
+test_unreal_runtime_properties!(app_default_locate, "AppDefaultLocale", "en-US");
+test_unreal_runtime_properties!(
+    build_version,
+    "BuildVersion",
+    "++UE4+Release-4.20-CL-4369336"
+);
+test_unreal_runtime_properties!(is_ue4_release, "IsUE4Release", true);
+test_unreal_runtime_properties!(username, "UserName", "bruno");
+test_unreal_runtime_properties!(
+    base_dir,
+    "BaseDir",
+    "//Mac/Home/Desktop/WindowsNoEditor/YetAnother/Binaries/Win64/"
+);
+test_unreal_runtime_properties!(root_dir, "RootDir", "/Mac/Home/Desktop/WindowsNoEditor/");
+test_unreal_runtime_properties!(machine_id, "MachineId", "9776D4844CC893F55395DBBEFB0EB6D7");
+test_unreal_runtime_properties!(login_id, "LoginId", "9776d4844cc893f55395dbbefb0eb6d7");
+test_unreal_runtime_properties!(epic_account_id, "EpicAccountId", "epic acc id");
+test_unreal_runtime_properties!(
+    legacy_call_stack,
+    "CallStack",
+    r#"YetAnother!AActor::IsPendingKillPending()
+YetAnother!__scrt_common_main_seh() [f:\dd\vctools\crt\vcstartup\src\startup\exe_common.inl:288]
+kernel32
+ntdll"#
+);
+test_unreal_runtime_properties!(portable_call_stack, "PCallStack", "YetAnother 0x0000000025ca0000 + 703394 YetAnother 0x0000000025ca0000 + 281f2ee YetAnother 0x0000000025ca0000 + 2a26dd3 YetAnother 0x0000000025ca0000 + 2a4f984 YetAnother 0x0000000025ca0000 + 355e77e YetAnother 0x0000000025ca0000 + 3576186 YetAnother 0x0000000025ca0000 + 8acc56 YetAnother 0x0000000025ca0000 + 8acf00 YetAnother 0x0000000025ca0000 + 35c121d YetAnother 0x0000000025ca0000 + 35cfb58 YetAnother 0x0000000025ca0000 + 2eb082f YetAnother 0x0000000025ca0000 + 2eb984f YetAnother 0x0000000025ca0000 + 2d1cd39 YetAnother 0x0000000025ca0000 + 325258 YetAnother 0x0000000025ca0000 + 334e4c YetAnother 0x0000000025ca0000 + 334eaa YetAnother 0x0000000025ca0000 + 3429e6 YetAnother 0x0000000025ca0000 + 44e73c6 KERNEL32 0x000000000fd40000 + 13034 ntdll 0x0000000010060000 + 71471");
+test_unreal_runtime_properties!(
+    error_message,
+    "ErrorMessage",
+    "Access violation - code c0000005 (first/second chance not available)"
+);
+test_unreal_runtime_properties!(crash_reporter_message, "CrashReporterMessage", "message");
+test_unreal_runtime_properties!(misc_number_of_cores, "Misc.NumberOfCores", 6);
+test_unreal_runtime_properties!(
+    misc_number_of_cores_inc_hyperthread,
+    "Misc.NumberOfCoresIncludingHyperthreads",
+    6
+);
+test_unreal_runtime_properties!(misc_is_64bit, "Misc.Is64bitOperatingSystem", true);
+test_unreal_runtime_properties!(misc_cpu_vendor, "Misc.CPUVendor", "GenuineIntel");
+test_unreal_runtime_properties!(
+    misc_cpu_brand,
+    "Misc.CPUBrand",
+    "Intel(R) Core(TM) i7-7920HQ CPU @ 3.10GHz"
+);
+test_unreal_runtime_properties!(
+    misc_primary_cpu_brand,
+    "Misc.PrimaryGPUBrand",
+    "Parallels Display Adapter (WDDM)"
+);
+test_unreal_runtime_properties!(misc_os_version_major, "Misc.OSVersionMajor", "Windows 10");
+test_unreal_runtime_properties!(
+    misc_os_version_minor,
+    "Misc.OSVersionMinor",
+    "some minor version"
+);
+test_unreal_runtime_properties!(game_state_name, "GameStateName", "game state");
+test_unreal_runtime_properties!(
+    memory_stats_total_physical,
+    "MemoryStats.TotalPhysical",
+    6896832512
+);
+test_unreal_runtime_properties!(
+    memory_stats_total_virtual,
+    "MemoryStats.TotalVirtual",
+    140737488224256
+);
+test_unreal_runtime_properties!(memory_stats_page_size, "MemoryStats.PageSize", 4096);
+test_unreal_runtime_properties!(
+    memory_stats_total_phsysical_gb,
+    "MemoryStats.TotalPhysicalGB",
+    7
+);
+test_unreal_runtime_properties!(time_of_crash, "TimeOfCrash", 636783195289630000);
+test_unreal_runtime_properties!(allowed_to_be_contacted, "bAllowToBeContacted", true);
+test_unreal_runtime_properties!(
+    crash_reporter_client_version,
+    "CrashReportClientVersion",
+    "1.0"
+);
+test_unreal_runtime_properties!(modules, "Modules", r#"\\Mac\Home\Desktop\WindowsNoEditor\YetAnother\Binaries\Win64\YetAnother.exe
+\\Mac\Home\Desktop\WindowsNoEditor\Engine\Binaries\ThirdParty\Vorbis\Win64\VS2015\libvorbis_64.dll"#);
 
 #[test]
 fn test_get_platform_properties() {
@@ -269,6 +426,7 @@ fn test_get_platform_properties() {
 <FGenericCrashContext>
 	<PlatformProperties>
 		<PlatformIsRunningWindows>0</PlatformIsRunningWindows>
+		<PlatformCallbackResult>123</PlatformCallbackResult>
 	</PlatformProperties>
 </FGenericCrashContext>
 "#
@@ -278,4 +436,5 @@ fn test_get_platform_properties() {
 
     let platform_properties = get_platform_properties(&root).expect("PlatformProperties exists");
     assert!(!platform_properties.is_windows.expect("is windows"));
+    assert_eq!(platform_properties.callback_result.expect("has value"), 123);
 }
