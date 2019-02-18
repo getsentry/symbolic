5bedbebb5edd14e8c62caa7bb940d6683117c818
Jan Michael Auer <jan.auer@sentry.io>
2019-02-18 12:58:58 +0100

    ref: Remove old code

--- common/src/cell.rs
+++ common/src/cell.rs
@@ -117,59 +117,6 @@ where
         SelfCell { owner, derived }
     }
 
-    // #[inline]
-    // pub fn map<U, F>(self, f: F) -> SelfCell<O, U>
-    // where
-    //     U: AsSelf<'slf>,
-    //     F: Fn(T) -> U,
-    // {
-    //     SelfCell {
-    //         owner: self.owner,
-    //         derived: f(self.derived),
-    //     }
-    // }
-
-    // #[inline]
-    // pub fn try_map<U, E, F>(self, f: F) -> Result<SelfCell<O, U>, E>
-    // where
-    //     U: AsSelf<'slf>,
-    //     F: Fn(T) -> Result<U, E>,
-    // {
-    //     Ok(SelfCell {
-    //         owner: self.owner,
-    //         derived: f(self.derived)?,
-    //     })
-    // }
-
-    // #[inline]
-    // #[allow(clippy::should_implement_trait)]
-    // pub fn as_ref(&self) -> SelfCell<O, &T>
-    // where
-    //     O: Clone,
-    // {
-    //     SelfCell {
-    //         owner: self.owner.clone(),
-    //         derived: &self.derived,
-    //     }
-    // }
-
-    // pub fn transform<F, U>(self, f: F) -> U
-    // where
-    //     F: FnOnce(&O, T) -> U,
-    // {
-    //     f(&self.owner, self.derived)
-    // }
-
-    // #[inline]
-    // pub fn clone_with<'b, U, F>(&self, f: F) -> SelfCell<O, U>
-    // where
-    //     O: Clone,
-    //     U: AsSelf<'b>,
-    //     F: FnOnce(&T) -> U,
-    // {
-    //     self.as_ref().map(f)
-    // }
-
     #[inline]
     pub fn owner(&self) -> &O {
         &self.owner
@@ -208,15 +155,6 @@ mod tests {
         assert_eq!(fooref.get().0, "hello world");
     }
 
-    // Should not compile due to lifetime conflict
-    // fn test_invalid() {
-    //     let outer = {
-    //         let fooref = SelfCell::new(String::from("hello world"), |s| Foo(unsafe { &*s }));
-    //         fooref.get().0
-    //     };
-    //     assert_eq!(outer, "hello world");
-    // }
-
     #[test]
     fn test_try_new() {
         let result = SelfCell::try_new(String::from("42"), |s| Foo::parse(unsafe { &*s }));
@@ -225,57 +163,4 @@ mod tests {
         let result = SelfCell::try_new(String::from("hello world"), |s| Foo::parse(unsafe { &*s }));
         result.expect_err("parsing should fail");
     }
-
-    // #[test]
-    // fn test_map() {
-    //     let fooref = SelfCell::new(String::from("  hello  "), Foo);
-    //     let mapped = fooref.map(|f| Foo(f.0.trim()));
-    //     assert_eq!(mapped.get().0, "hello");
-    // }
-
-    // #[test]
-    // fn test_map_invalid() {
-    //     let mut outer = None;
-    //     let fooref = SelfCell::new(String::from("  hello  "), |s| {
-    //         outer = Some(s);
-    //         Foo(s)
-    //     });
-    //     std::mem::drop(fooref)
-    //     // TODO(ja): this is a use after free
-    //     assert_eq!("  hello  ", outer.unwrap());
-    // }
-
-    // #[test]
-    // fn test_try_map() {
-    //     let fooref = SelfCell::new(String::from("  42  "), Foo);
-    //     let result = fooref.try_map(|f| Foo::parse(f.0.trim()));
-    //     result.expect("parsing should not fail");
-
-    //     let fooref = SelfCell::new(String::from("  hello  "), Foo);
-    //     let result = fooref.try_map(|f| Foo::parse(f.0.trim()));
-    //     result.expect_err("parsing should fail");
-    // }
-
-    // #[test]
-    // fn test_as_ref() {
-    //     let fooref = SelfCell::new(String::from("hello world"), Foo);
-    //     let foorefref = fooref.as_ref();
-    //     assert_eq!(foorefref.get(), fooref.get());
-    //     assert_eq!(
-    //         foorefref.get() as *const Foo<'_>,
-    //         fooref.get() as *const Foo<'_>
-    //     );
-    // }
-
-    // #[test]
-    // fn test_split() {
-    //     let fooref = SelfCell::new(String::from("helloworld"), Foo);
-    //     let (hello, world) = fooref.transmute(|string, foo| {
-    //         let (hello, world) = fooref.get().split_at(5);
-    //         (
-    //             SelfCell::new(string.clone(), hello),
-    //             SelfCell::new(string.clone(), world),
-    //         )
-    //     });
-    // }
 }
