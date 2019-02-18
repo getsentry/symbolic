4363a8a5470cb2374db4c47588a32a5a3c6959d2
Jan Michael Auer <jan.auer@sentry.io>
2019-02-18 10:05:51 +0100

    ref: Check in top-level modules

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,15 @@
 pub use symbolic_common as common;
 #[cfg(feature = "debuginfo")]
 pub use symbolic_debuginfo as debuginfo;
-// #[cfg(feature = "demangle")]
-// pub use symbolic_demangle as demangle;
-// #[cfg(feature = "minidump")]
-// pub use symbolic_minidump as minidump;
-// #[cfg(feature = "proguard")]
-// pub use symbolic_proguard as proguard;
-// #[cfg(feature = "sourcemap")]
-// pub use symbolic_sourcemap as sourcemap;
-// #[cfg(feature = "symcache")]
-// pub use symbolic_symcache as symcache;
-// #[cfg(feature = "unreal")]
-// pub use symbolic_unreal as unreal;
+#[cfg(feature = "demangle")]
+pub use symbolic_demangle as demangle;
+#[cfg(feature = "minidump")]
+pub use symbolic_minidump as minidump;
+#[cfg(feature = "proguard")]
+pub use symbolic_proguard as proguard;
+#[cfg(feature = "sourcemap")]
+pub use symbolic_sourcemap as sourcemap;
+#[cfg(feature = "symcache")]
+pub use symbolic_symcache as symcache;
+#[cfg(feature = "unreal")]
+pub use symbolic_unreal as unreal;
