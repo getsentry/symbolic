ec0a76d8261c17444e55e56a7e600e348b801af5
Jan Michael Auer <jan.auer@sentry.io>
2019-03-04 09:19:09 +0100

    doc: Describe differences to the object crate

--- debuginfo/src/lib.rs
+++ debuginfo/src/lib.rs
@@ -1,9 +1,9 @@
 //! Abstractions for dealing with object files and debug information.
 //!
-//! This crate defines the [`Object`] type, which is an abstraction over various object file formats
-//! used in different platforms. Also, since executables on MacOS might contain multiple object
-//! files (called a _"Fat MachO"_), there is an [`Archive`] type, that provides a uniform interface
-//! with access to an objects iterator in all platforms.
+//! This module defines the [`Object`] type, which is an abstraction over various object file
+//! formats used in different platforms. Also, since executables on MacOS might contain multiple
+//! object files (called a _"Fat MachO"_), there is an [`Archive`] type, that provides a uniform
+//! interface with access to an objects iterator in all platforms.
 //!
 //! Most processing of object files will happen on the `Object` type or its concrete implementation
 //! for one platform. To allow abstraction over this, there is the [`ObjectLike`] trait. It defines
@@ -13,10 +13,25 @@
 //! See [`Object`] for the full API, or use one of the modules for direct access to the
 //! platform-dependent data.
 //!
+//! # Background
+//!
+//! The functionality of `symbolic::debuginfo` is conceptionally similar to the [`object`] crate.
+//! However, there are key differences that warranted a separate implementation:
+//!
+//!  - `object` has a stronger focus on executable formats, while `symbolic` focusses on debugging
+//!    information. This is why `symbolic` also includes a variant for PDBs and Breakpad objects,
+//!    where `object` instead has a WASM variant.
+//!  - `object` contains far more generic access to the data within objects at the cost of
+//!    performance. `symbolic` tries to optimize for debugging scenarios at the cost of generic
+//!    usage.
+//!  - `symbolic` contains an abstraction for multi-object files ([`Archive`]), which is not easily
+//!    possible in `object` due to the use of lifetimes on the `object::Object` trait.
+//!
 //! [`Object`]: enum.Object.html
 //! [`Archive`]: enum.Archive.html
 //! [`ObjectLike`]: trait.ObjectLike.html
 //! [`DebugSession`]: trait.DebugSession.html
+//! [`object`]: https://docs.rs/object
 
 #![warn(missing_docs)]
 
