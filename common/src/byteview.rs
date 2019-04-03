926425f5e54fcedfbff6c3ee60151716065ed7d8
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-04-03 12:19:01 +0200

    fix: Reference correct self

--- common/src/byteview.rs
+++ common/src/byteview.rs
@@ -113,7 +113,7 @@ impl<'a> ByteView<'a> {
     /// Constructs a `ByteView` from a file path by memory mapping the file.
     pub fn open<P: AsRef<Path>>(path: P) -> Result<Self, io::Error> {
         let file = File::open(path)?;
-        Ok(self.from_file(file))
+        Ok(Self::from_file(file))
     }
 
     /// Returns a slice of the underlying data.
