961c1a4cfe20223c35379c46af1920669d60e9ed
Jan Michael Auer <jan.auer@sentry.io>
2019-04-25 17:39:59 +0200

    build(demangle): Disable unused bits of libswift demangle

--- demangle/build.rs
+++ demangle/build.rs
@@ -9,10 +9,10 @@ fn main() {
             "vendor/swift/lib/Demangling/NodeDumper.cpp",
             "vendor/swift/lib/Demangling/NodePrinter.cpp",
             "vendor/swift/lib/Demangling/OldDemangler.cpp",
-            "vendor/swift/lib/Demangling/OldRemangler.cpp",
+            // "vendor/swift/lib/Demangling/OldRemangler.cpp",
             "vendor/swift/lib/Demangling/Punycode.cpp",
-            "vendor/swift/lib/Demangling/Remangler.cpp",
-            "vendor/swift/lib/Demangling/TypeDecoder.cpp",
+            // "vendor/swift/lib/Demangling/Remangler.cpp",
+            // "vendor/swift/lib/Demangling/TypeDecoder.cpp",
         ])
         .flag("-std=c++11")
         .flag("-DLLVM_DISABLE_ABI_BREAKING_CHECKS_ENFORCING=1")
