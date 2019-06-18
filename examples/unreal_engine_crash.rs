018a633d11a1ab5c733210c719e90c9dfaed4ed0
Jan Michael Auer <jan.auer@sentry.io>
2019-06-18 19:44:51 +0200

    ref(example): Update the unreal example

--- examples/unreal_engine_crash.rs
+++ examples/unreal_engine_crash.rs
@@ -5,7 +5,7 @@ use std::io::Read;
 use clap::{App, Arg, ArgMatches};
 use failure::Error;
 
-use symbolic::unreal::Unreal4Crash;
+use symbolic::unreal::{Unreal4Crash, Unreal4FileType};
 
 fn execute(matches: &ArgMatches) -> Result<(), Error> {
     let crash_file_path = matches.value_of("crash_file_path").unwrap();
@@ -14,25 +14,24 @@ fn execute(matches: &ArgMatches) -> Result<(), Error> {
     let mut file_content = Vec::new();
     file.read_to_end(&mut file_content)?;
 
-    let ue4_crash = Unreal4Crash::from_slice(&file_content)?;
+    let ue4_crash = Unreal4Crash::parse(&file_content)?;
 
-    match ue4_crash.get_minidump_slice()? {
-        Some(m) => println!("Minidump size: {} bytes.", m.len()),
+    match ue4_crash.file_by_type(Unreal4FileType::Minidump) {
+        Some(m) => println!("Minidump size: {} bytes.", m.data().len()),
         None => println!("No minidump found in the Unreal Crash provided."),
     }
 
-    for file_meta in ue4_crash.files() {
-        let contents = &ue4_crash.get_file_contents(file_meta)?;
+    for file in ue4_crash.files() {
         println!(
             "File name: {:?}, Type: {:?}, size: {:?}, preview {:?}",
-            file_meta.file_name,
-            file_meta.ty(),
-            file_meta.len,
-            String::from_utf8_lossy(&contents[..cmp::min(50, contents.len())])
+            file.name(),
+            file.ty(),
+            file.data().len(),
+            String::from_utf8_lossy(&file.data()[..cmp::min(50, file.data().len())])
         );
     }
 
-    for log in ue4_crash.get_logs(1000).unwrap().iter() {
+    for log in ue4_crash.logs(1000).unwrap().iter() {
         println!(
             "{:?} - {:?} - {:?}",
             log.timestamp, log.component, log.message
