2f91158ab94ee5a95d3e751f26a613d0fdbb5d92
Jan Michael Auer <jan.auer@sentry.io>
2019-06-26 15:54:01 +0200

    ref(example): Update dump_sources to updated interface

--- examples/dump_sources.rs
+++ examples/dump_sources.rs
@@ -28,9 +28,8 @@ fn write_object_sources(path: &Path, output_path: &Path) -> Result<(), Error> {
             Ok(object) => {
                 let out = output_path.join(&format!("{}.zip", &object.debug_id()));
                 println!("  -> {}", out.display());
-                let mut writer = SourceBundleWriter::create(&out)?;
-                writer.add_object(&object, &path.file_name().unwrap().to_string_lossy())?;
-                writer.finish()?;
+                let writer = SourceBundleWriter::create(&out)?;
+                writer.write_object(&object, &path.file_name().unwrap().to_string_lossy())?;
             }
             Err(e) => {
                 print!(" - ");
