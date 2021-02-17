4059b60ff8ac80640846359e8da036632f06550e
Jan Michael Auer <mail@jauer.org>
2021-02-17 14:19:39 +0100

    build: Drop support for python 2.7

--- py/manylinux.sh
+++ py/manylinux.sh
@@ -13,7 +13,7 @@ if [ "$AUDITWHEEL_ARCH" == "i686" ]; then
   LINUX32=linux32
 fi
 
-$LINUX32 /opt/python/cp27-cp27mu/bin/python setup.py bdist_wheel
+$LINUX32 /opt/python/cp37-cp37m/bin/python setup.py bdist_wheel
 
 # Audit wheels
 for wheel in dist/*-linux_*.whl; do
