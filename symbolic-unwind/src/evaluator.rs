be810fe3e7e09f58a969ad8955d2e75faa30178e
Sebastian Zivota <loewenheim@mailbox.org>
2021-02-26 15:00:22 +0100

    feat(unwind): Add FromStr impls for various types

--- symbolic-unwind/src/evaluator.rs
+++ symbolic-unwind/src/evaluator.rs
@@ -28,6 +28,7 @@
 //! insertion if it was not defined before.
 //!
 use super::base::{Endianness, MemoryRegion, RegisterValue};
+use parsing::ExprParsingError;
 use std::collections::{BTreeMap, BTreeSet};
 use std::fmt;
 use std::str::FromStr;
@@ -129,9 +130,9 @@ impl<A: RegisterValue + FromStr, M: MemoryRegion, E: Endianness> Evaluator<M, A,
     pub fn process<'a>(
         &'a mut self,
         input: &'a str,
-    ) -> Result<BTreeSet<Variable>, ExpressionError<'a, A>> {
+    ) -> Result<BTreeSet<Variable>, ExpressionError<&'a str, A>> {
         let mut changed_variables = BTreeSet::new();
-        let assignments = parsing::assignments::<A>(input)?;
+        let assignments = parsing::assignments_complete::<A>(input)?;
         for a in assignments {
             self.assign(&a)?;
             changed_variables.insert(a.0);
@@ -165,21 +166,21 @@ pub enum EvaluationError<A: RegisterValue> {
 
 /// An error encountered while parsing or evaluating an expression.
 #[derive(Debug)]
-pub enum ExpressionError<'a, A: RegisterValue> {
+pub enum ExpressionError<I, A: RegisterValue> {
     /// An error was encountered while parsing an expression.
-    Parsing(parsing::ExprParsingError<'a>),
+    Parsing(ExprParsingError<I>),
 
     /// An error was encountered while evaluating an expression.
     Evaluation(EvaluationError<A>),
 }
 
-impl<'a, A: RegisterValue> From<parsing::ExprParsingError<'a>> for ExpressionError<'a, A> {
-    fn from(other: parsing::ExprParsingError<'a>) -> Self {
+impl<I, A: RegisterValue> From<ExprParsingError<I>> for ExpressionError<I, A> {
+    fn from(other: ExprParsingError<I>) -> Self {
         Self::Parsing(other)
     }
 }
 
-impl<'a, A: RegisterValue> From<EvaluationError<A>> for ExpressionError<'a, A> {
+impl<I, A: RegisterValue> From<EvaluationError<A>> for ExpressionError<I, A> {
     fn from(other: EvaluationError<A>) -> Self {
         Self::Evaluation(other)
     }
@@ -195,6 +196,17 @@ impl fmt::Display for Variable {
     }
 }
 
+impl FromStr for Variable {
+    type Err = ExprParsingError<String>;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        parsing::variable_complete(input).map_err(|e| ExprParsingError {
+            kind: e.kind,
+            input: e.input.to_string(),
+        })
+    }
+}
+
 /// A constant value.
 #[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct Constant(String);
@@ -202,6 +214,17 @@ pub struct Constant(String);
 impl fmt::Display for Constant {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         self.0.fmt(f)
+    }
+}
+
+impl FromStr for Constant {
+    type Err = ExprParsingError<String>;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        parsing::constant_complete(input).map_err(|e| ExprParsingError {
+            kind: e.kind,
+            input: e.input.to_string(),
+        })
     }
 }
 
@@ -266,6 +289,17 @@ impl<T: fmt::Display> fmt::Display for Expr<T> {
     }
 }
 
+impl<T: FromStr> FromStr for Expr<T> {
+    type Err = ExprParsingError<String>;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        parsing::expr_complete(input).map_err(|e| ExprParsingError {
+            kind: e.kind,
+            input: e.input.to_string(),
+        })
+    }
+}
+
 /// An assignment `v e =` where `v` is a [variable](Variable) and `e` is an [expression](Expr).
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Assignment<T>(Variable, Expr<T>);
@@ -273,6 +307,17 @@ pub struct Assignment<T>(Variable, Expr<T>);
 impl<T: fmt::Display> fmt::Display for Assignment<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{} {} =", self.0, self.1)
+    }
+}
+
+impl<T: FromStr> FromStr for Assignment<T> {
+    type Err = ExprParsingError<String>;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        parsing::assignment_complete(input).map_err(|e| ExprParsingError {
+            kind: e.kind,
+            input: e.input.to_string(),
+        })
     }
 }
 
@@ -307,32 +352,37 @@ pub mod parsing {
         /// More than one expression preceded a `=`.
         MalformedAssignment,
 
+        /// Only one expression was expected, but multiple were parsed.
+        TooManyExpressions,
+
         /// An error returned by `nom`.
         Nom(nom::error::ErrorKind),
     }
 
     /// An error encountered while parsing expressions.
     #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-    pub struct ExprParsingError<'a> {
-        kind: ExprParsingErrorKind,
-        input: &'a str,
+    pub struct ExprParsingError<I> {
+        /// The kind of error.
+        pub kind: ExprParsingErrorKind,
+        /// The input that caused the error.
+        pub input: I,
     }
 
-    impl<'a> ParseError<&'a str> for ExprParsingError<'a> {
-        fn from_error_kind(input: &'a str, kind: nom::error::ErrorKind) -> Self {
+    impl<I> ParseError<I> for ExprParsingError<I> {
+        fn from_error_kind(input: I, kind: nom::error::ErrorKind) -> Self {
             Self {
                 input,
                 kind: ExprParsingErrorKind::Nom(kind),
             }
         }
 
-        fn append(_input: &'a str, _kind: nom::error::ErrorKind, other: Self) -> Self {
+        fn append(_input: I, _kind: nom::error::ErrorKind, other: Self) -> Self {
             other
         }
     }
 
-    impl<'a, E> nom::error::FromExternalError<&'a str, E> for ExprParsingError<'a> {
-        fn from_external_error(input: &'a str, kind: nom::error::ErrorKind, _e: E) -> Self {
+    impl<I, E> nom::error::FromExternalError<I, E> for ExprParsingError<I> {
+        fn from_external_error(input: I, kind: nom::error::ErrorKind, _e: E) -> Self {
             Self::from_error_kind(input, kind)
         }
     }
@@ -340,15 +390,23 @@ pub mod parsing {
     /// Parses a [variable](super::Variable).
     ///
     /// This accepts identifiers of the form `$[a-zA-Z][a-zA-Z0-9]*`.
-    fn variable(input: &str) -> IResult<&str, Variable, ExprParsingError> {
+    fn variable(input: &str) -> IResult<&str, Variable, ExprParsingError<&str>> {
         let (rest, var) = recognize(tuple((char('$'), alpha1, alphanumeric0)))(input)?;
         Ok((rest, Variable(var.to_string())))
+    }
+
+    /// Parses a [variable](super::Variable).
+    ///
+    /// This accepts identifiers of the form `$[a-zA-Z][a-zA-Z0-9]*`.
+    /// It will fail if there is any non-whitespace input remaining afterwards.
+    pub fn variable_complete(input: &str) -> Result<Variable, ExprParsingError<&str>> {
+        all_consuming(variable)(input).finish().map(|(_, v)| v)
     }
 
     /// Parses a [constant](super::Constant).
     ///
     /// This accepts identifiers of the form `[a-zA-Z_.][a-zA-Z0-9_.]*`.
-    fn constant(input: &str) -> IResult<&str, Constant, ExprParsingError> {
+    fn constant(input: &str) -> IResult<&str, Constant, ExprParsingError<&str>> {
         let (rest, con) = recognize(preceded(
             alt((alpha1, tag("_"), tag("."))),
             many0(alt((alphanumeric1, tag("_"), tag(".")))),
@@ -356,8 +414,16 @@ pub mod parsing {
         Ok((rest, Constant(con.to_string())))
     }
 
+    /// Parses a [constant](super::Constant).
+    ///
+    /// This accepts identifiers of the form `[a-zA-Z_.][a-zA-Z0-9_.]*`.
+    /// It will fail if there is any non-whitespace input remaining afterwards.
+    pub fn constant_complete(input: &str) -> Result<Constant, ExprParsingError<&str>> {
+        all_consuming(constant)(input).finish().map(|(_, c)| c)
+    }
+
     /// Parses a [binary operator](super::BinOp).
-    fn bin_op(input: &str) -> IResult<&str, BinOp, ExprParsingError> {
+    fn bin_op(input: &str) -> IResult<&str, BinOp, ExprParsingError<&str>> {
         alt((
             value(BinOp::Add, tag("+")),
             value(BinOp::Sub, tag("-")),
@@ -371,14 +437,14 @@ pub mod parsing {
     /// Parses an integer.
     ///
     /// This accepts expressions of the form `-?[0-9]+`.
-    fn number<T: FromStr>(input: &str) -> IResult<&str, T, ExprParsingError> {
+    fn number<T: FromStr>(input: &str) -> IResult<&str, T, ExprParsingError<&str>> {
         map_res(recognize(pair(opt(tag("-")), digit1)), |s: &str| {
             s.parse::<T>()
         })(input)
     }
 
     /// Parses a number, variable, or constant.
-    fn base_expr<T: FromStr>(input: &str) -> IResult<&str, Expr<T>, ExprParsingError> {
+    fn base_expr<T: FromStr>(input: &str) -> IResult<&str, Expr<T>, ExprParsingError<&str>> {
         alt((
             map(number, Expr::Value),
             map(variable, Expr::Var),
@@ -386,27 +452,29 @@ pub mod parsing {
         ))(input)
     }
 
-    /// Parses a stack of expressions.
+    /// Parses a stack of [expressions](super::Expression).
     ///
     /// # Example
     /// ```rust
     /// use symbolic_unwind::evaluator::Expr::*;
     /// use symbolic_unwind::evaluator::BinOp::*;
-    /// # use symbolic_unwind::evaluator::parsing::expr;
+    /// # use symbolic_unwind::evaluator::parsing::expr_stack;
     ///
-    /// let (_, stack) = expr("1 2 + 3").unwrap();
+    /// let (_, stack) = expr_stack("1 2 + 3").unwrap();
     /// assert_eq!(stack.len(), 2);
     /// assert_eq!(stack[0], Op(Box::new(Value(1)), Box::new(Value(2)), Add));
     /// assert_eq!(stack[1], Value(3));
     /// ```
-    pub fn expr<T: FromStr>(mut input: &str) -> IResult<&str, Vec<Expr<T>>, ExprParsingError> {
+    pub fn expr_stack<T: FromStr>(
+        mut input: &str,
+    ) -> IResult<&str, Vec<Expr<T>>, ExprParsingError<&str>> {
         let mut stack = Vec::new();
 
         while !input.is_empty() {
             if let Ok((rest, e)) = delimited(multispace0, base_expr, multispace0)(input) {
                 stack.push(e);
                 input = rest;
-            } else if let Ok((rest, _)) = delimited::<_, _, _, _, ExprParsingError, _, _, _>(
+            } else if let Ok((rest, _)) = delimited::<_, _, _, _, ExprParsingError<&str>, _, _, _>(
                 multispace0,
                 tag("^"),
                 multispace0,
@@ -454,10 +522,26 @@ pub mod parsing {
         Ok((input, stack))
     }
 
-    /// Parses an [assignment](Assignment).
-    pub fn assignment<T: FromStr>(input: &str) -> IResult<&str, Assignment<T>, ExprParsingError> {
+    /// Parses an [expression](super::Expr).
+    ///
+    /// It will fail if there is any non-whitespace input remaining afterwards.
+    pub fn expr_complete<T: FromStr>(input: &str) -> Result<Expr<T>, ExprParsingError<&str>> {
+        let (_, mut stack) = all_consuming(expr_stack)(input).finish()?;
+        if stack.len() > 1 {
+            Err(ExprParsingError {
+                kind: ExprParsingErrorKind::TooManyExpressions,
+                input,
+            })
+        } else {
+            // This unwrap cannot fail: if the parser succeded, the stack is nonempty.
+            Ok(stack.pop().unwrap())
+        }
+    }
+
+    /// Parses an [assignment](super::Assignment).
+    fn assignment<T: FromStr>(input: &str) -> IResult<&str, Assignment<T>, ExprParsingError<&str>> {
         let (input, v) = delimited(multispace0, variable, multispace0)(input)?;
-        let (input, mut stack) = expr(input)?;
+        let (input, mut stack) = expr_stack(input)?;
 
         // At this point there should be exactly one expression on the stack, otherwise
         // it's not a well-formed assignment.
@@ -482,12 +566,21 @@ pub mod parsing {
         Ok((rest, Assignment(v, e)))
     }
 
-    /// Parses a list of assignments.
+    /// Parses an [assignment](super::Assignment).
     ///
-    /// Will fail if there is any non-whitespace input remaining afterwards.
-    pub fn assignments<T: FromStr + std::fmt::Debug>(
+    /// It will fail if there is any non-whitespace input remaining afterwards.
+    pub fn assignment_complete<T: FromStr>(
         input: &str,
-    ) -> Result<Vec<Assignment<T>>, ExprParsingError> {
+    ) -> Result<Assignment<T>, ExprParsingError<&str>> {
+        all_consuming(assignment)(input).finish().map(|(_, a)| a)
+    }
+
+    /// Parses a sequence of [assignments](super::Assignment).
+    ///
+    /// It will fail if there is any non-whitespace input remaining afterwards.
+    pub fn assignments_complete<T: FromStr + std::fmt::Debug>(
+        input: &str,
+    ) -> Result<Vec<Assignment<T>>, ExprParsingError<&str>> {
         let (_, assigns) =
             all_consuming(many0(delimited(multispace0, assignment, multispace0)))(input)
                 .finish()?;
@@ -508,7 +601,7 @@ pub mod parsing {
                 Box::new(Value(-3)),
                 BinOp::Mul,
             );
-            let (rest, parsed) = expr(input).unwrap();
+            let (rest, parsed) = expr_stack(input).unwrap();
             assert_eq!(rest, "");
             assert_eq!(parsed, vec![e]);
         }
@@ -536,7 +629,7 @@ pub mod parsing {
                 Box::new(Var(Variable(String::from("$foo")))),
                 BinOp::Mul,
             );
-            let (rest, parsed) = expr(input).unwrap();
+            let (rest, parsed) = expr_stack(input).unwrap();
             assert_eq!(rest, "");
             assert_eq!(parsed, vec![e1, e2]);
         }
@@ -544,7 +637,7 @@ pub mod parsing {
         #[test]
         fn test_expr_malformed() {
             let input = "3 +";
-            let err = expr::<i8>(input).finish().unwrap_err();
+            let err = expr_stack::<i8>(input).finish().unwrap_err();
             assert_eq!(
                 err,
                 ExprParsingError {
@@ -642,8 +735,8 @@ mod test {
             constants: BTreeMap::new(),
             endian: BigEndian,
         };
-        let r_add3 = Variable("$rAdd3".to_string());
-        let r_mul2 = Variable("$rMul2".to_string());
+        let r_add3: Variable = "$rAdd3".parse().unwrap();
+        let r_mul2: Variable = "$rMul2".parse().unwrap();
 
         let changed_vars = eval.process(input).unwrap();
 
@@ -667,7 +760,7 @@ mod test {
             endian: BigEndian,
         };
 
-        let r_deref = Variable("$rDeref".to_string());
+        let r_deref: Variable = "$rDeref".parse().unwrap();
 
         let changed_vars = eval.process(input).unwrap();
 
@@ -678,15 +771,15 @@ mod test {
 
     #[test]
     fn test_intermediate() {
-        let ebp = Variable("$ebp".to_string());
-        let eip = Variable("$eip".to_string());
-        let esp = Variable("$esp".to_string());
-        let ebx = Variable("$ebx".to_string());
-        let t0 = Variable("$T0".to_string());
-        let t1 = Variable("$T1".to_string());
-        let t2 = Variable("$T2".to_string());
-        let l = Variable("$L".to_string());
-        let p = Variable("$P".to_string());
+        let ebp: Variable = "$ebp".parse().unwrap();
+        let eip: Variable = "$eip".parse().unwrap();
+        let esp: Variable = "$esp".parse().unwrap();
+        let ebx: Variable = "$ebx".parse().unwrap();
+        let t0: Variable = "$T0".parse().unwrap();
+        let t1: Variable = "$T1".parse().unwrap();
+        let t2: Variable = "$T2".parse().unwrap();
+        let l: Variable = "$L".parse().unwrap();
+        let p: Variable = "$P".parse().unwrap();
 
         let variables = vec![
             (ebp.clone(), 0xbfff_0010),
