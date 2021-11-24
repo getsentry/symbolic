6eb3f91d8e562af331922d94f10ddb3271ed2466
Arpad Borsos <swatinem@swatinem.de>
2021-11-24 12:12:53 +0100

    feat: Add new raw SymCache Format
    This includes the raw serialized structs, and the SymCache parser.

--- symbolic-symcache/src/lib.rs
+++ symbolic-symcache/src/lib.rs
@@ -1,7 +1,10 @@
-//! Provides symcache support.
+//! Provides SymCache support.
 
 #![warn(missing_docs)]
 
+// TODO: temporarily, since usage will be added later on.
+#[allow(dead_code)]
+mod new;
 mod old;
 
 pub use old::*;
