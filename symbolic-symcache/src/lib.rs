a92df947354f3800a72c63d91a0f4924d443caa7
Sebastian Zivota <loewenheim@mailbox.org>
2021-12-01 12:42:04 +0100

    Expose new symache version

--- symbolic-symcache/src/lib.rs
+++ symbolic-symcache/src/lib.rs
@@ -8,5 +8,6 @@ mod old;
 pub(crate) mod preamble;
 
 pub use compat::*;
+pub use new::raw::SYMCACHE_VERSION;
 pub use new::SymCacheWriter;
 pub use old::{format, Line, LineInfo, SymCacheError, SymCacheErrorKind, ValueKind};
