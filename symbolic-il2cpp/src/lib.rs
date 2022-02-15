ce164dc799ea4cf1af1b2594b587ea9f2a5b59ba
Arpad Borsos <swatinem@swatinem.de>
2022-02-15 16:41:38 +0100

    correlate metadata back to dwarf info

--- symbolic-il2cpp/src/lib.rs
+++ symbolic-il2cpp/src/lib.rs
@@ -1,3 +1,7 @@
+use std::borrow;
+use std::collections::BTreeMap;
+
+use object::{Object, ObjectSection};
 use scroll::Pread;
 
 const IL2CPP_METADATA_MAGIC: u32 = 0xFAB11BAF; // TODO: use from_bytes_be
@@ -20,6 +24,129 @@ impl Il2CppMetadata {
         }
 
         Ok(Self {})
+    }
+}
+
+#[derive(Debug)]
+struct DwarfData {
+    /// A map from function pointer (DW_AT_low_pc) to function name (DW_AT_name).
+    functions: BTreeMap<u64, String>,
+    /// The offset of `g_CodeGenModules` (DW_TAG_variable) in the corresponding executable file.
+    codegenmodules_offset: Option<u64>,
+}
+
+impl DwarfData {
+    pub fn parse<R>(dwarf: &gimli::Dwarf<R>) -> anyhow::Result<Self>
+    where
+        R: gimli::Reader + std::ops::Deref<Target = [u8]> + PartialEq,
+    {
+        let mut functions = BTreeMap::new();
+        let mut codegenmodules_offset = None;
+
+        // Iterate over the compilation units.
+        let mut iter = dwarf.units();
+        while let Some(header) = iter.next()? {
+            let unit = dwarf.unit(header)?;
+
+            // Iterate over the Debugging Information Entries (DIEs) in the unit.
+            let mut _depth = 0;
+            let mut entries = unit.entries();
+            while let Some((delta_depth, entry)) = entries.next_dfs()? {
+                _depth += delta_depth;
+                // println!("<{}><{:x}> {}", depth, entry.offset().0, entry.tag());
+
+                let mut name = None;
+                let mut low_pc = None;
+                let mut location = None;
+
+                // Iterate over the attributes in the DIE.
+                let mut attrs = entry.attrs();
+                while let Some(attr) = attrs.next()? {
+                    match attr.name() {
+                        gimli::constants::DW_AT_name => {
+                            let attr_name = dwarf.attr_string(&unit, attr.value())?;
+                            // TODO: this allocates all the time because of lifetime issues:
+                            name = Some(std::str::from_utf8(&attr_name)?.to_string());
+                        }
+                        gimli::constants::DW_AT_low_pc => {
+                            if let gimli::read::AttributeValue::Addr(addr) = attr.value() {
+                                low_pc = Some(addr);
+                            }
+                        }
+                        gimli::constants::DW_AT_location => {
+                            location = attr.exprloc_value();
+                        }
+                        _ => {}
+                    }
+                }
+
+                if let Some(name) = name {
+                    if name == "g_CodeGenModules" {
+                        if let Some(expr) = location {
+                            let mut eval = expr.evaluation(unit.encoding());
+                            let mut result = eval.evaluate().unwrap();
+                            while result != gimli::EvaluationResult::Complete {
+                                match result {
+                                    gimli::EvaluationResult::RequiresRelocatedAddress(addr) => {
+                                        result = eval.resume_with_relocated_address(addr).unwrap();
+                                    }
+
+                                    _ => break, // TODO: implement more cases
+                                };
+                            }
+
+                            if result == gimli::EvaluationResult::Complete {
+                                for res in eval.as_result() {
+                                    if let gimli::Location::Address { address } = res.location {
+                                        codegenmodules_offset = Some(address);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if let Some(low_pc) = low_pc {
+                        if entry.tag() == gimli::constants::DW_TAG_subprogram {
+                            functions.insert(low_pc, name);
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(Self {
+            functions,
+            codegenmodules_offset,
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct Il2CppCodeGenModule<'d> {
+    name: &'d str,
+    method_pointers: &'d [u64], // TODO: this needs to be generic
+}
+
+impl<'d> Il2CppCodeGenModule<'d> {
+    pub fn parse(buf: &'d [u8], mut offset: usize) -> anyhow::Result<Self> {
+        let offset = &mut offset;
+
+        let name_ptr = buf.gread::<u64>(offset)? as usize;
+        let name =
+            buf.pread_with::<&str>(name_ptr, scroll::ctx::StrCtx::Delimiter(scroll::ctx::NULL))?;
+
+        let num_methods = buf.gread::<u64>(offset)? as usize;
+        let methods_ptr = buf.gread::<u64>(offset)? as usize;
+
+        // TODO: turn this into a safe iter instead
+        let method_pointers = unsafe {
+            let raw_buf = buf.as_ptr().add(methods_ptr);
+            std::slice::from_raw_parts(raw_buf as *const u64, num_methods)
+        };
+
+        Ok(Self {
+            name,
+            method_pointers,
+        })
     }
 }
 
@@ -56,24 +183,77 @@ mod tests {
         let dylib_path = fixtures_dir.join("IL2CPP.app/Contents/Frameworks/GameAssembly.dylib");
         let dylib_file = File::open(dylib_path).unwrap();
         let dylib_buf = unsafe { memmap2::Mmap::map(&dylib_file) }.unwrap();
-        let dylib = object::File::parse(first_arch(&dylib_buf)).unwrap();
+        let dylib_arch_buf = first_arch(&dylib_buf);
+        let dylib = object::File::parse(dylib_arch_buf).unwrap();
 
         let dsym_path = fixtures_dir.join("IL2CPP_BackUpThisFolder_ButDontShipItWithYourGame/GameAssembly.dSYM/Contents/Resources/DWARF/GameAssembly.dylib");
         let dsym_file = File::open(dsym_path).unwrap();
         let dsym_buf = unsafe { memmap2::Mmap::map(&dsym_file) }.unwrap();
-        let dsym = object::File::parse(first_arch(&dsym_buf)).unwrap();
+        let dsym_arch_buf = first_arch(&dsym_buf);
+        let dsym = object::File::parse(dsym_arch_buf).unwrap();
+
+        let object = &dsym;
+
+        let endian = if object.is_little_endian() {
+            gimli::RunTimeEndian::Little
+        } else {
+            gimli::RunTimeEndian::Big
+        };
+
+        // Load a section and return as `Cow<[u8]>`.
+        let load_section = |id: gimli::SectionId| -> Result<borrow::Cow<[u8]>, gimli::Error> {
+            match object.section_by_name(id.name()) {
+                Some(ref section) => Ok(section
+                    .uncompressed_data()
+                    .unwrap_or(borrow::Cow::Borrowed(&[][..]))),
+                None => Ok(borrow::Cow::Borrowed(&[][..])),
+            }
+        };
+
+        // Load all of the sections.
+        let dwarf_cow = gimli::Dwarf::load(&load_section).unwrap();
+
+        // Borrow a `Cow<[u8]>` to create an `EndianSlice`.
+        let borrow_section: &dyn for<'a> Fn(
+            &'a borrow::Cow<[u8]>,
+        )
+            -> gimli::EndianSlice<'a, gimli::RunTimeEndian> =
+            &|section| gimli::EndianSlice::new(&*section, endian);
+
+        // Create `EndianSlice`s for all of the sections.
+        let dwarf = dwarf_cow.borrow(&borrow_section);
+
+        let dwarf_data = DwarfData::parse(&dwarf).unwrap();
+        // dbg!(&dwarf_data);
 
         // Builds/IL2CPP_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppCodeRegistration.cpp
         // IL2CPP_EXTERN_C const Il2CppCodeGenModule* g_CodeGenModules[];
-        let codegenmodules_addr = find_symbol_addr(&dsym, b"g_CodeGenModules")
-            .unwrap()
-            .unwrap();
+        // let codegenmodules_addr = find_symbol_addr(&dsym, b"g_CodeGenModules")
+        //     .unwrap()
+        //     .unwrap();
 
         // Builds/IL2CPP_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
         // IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
-        let assembly_addr = find_symbol_addr(&dsym, b"g_AssemblyU2DCSharp_CodeGenModule");
+        // let assembly_addr = find_symbol_addr(&dsym, b"g_AssemblyU2DCSharp_CodeGenModule")
+        //     .unwrap()
+        //     .unwrap();
 
-        dbg!(codegenmodules_addr, assembly_addr);
+        // dbg!(codegenmodules_addr, assembly_addr);
+        // 7210096
+        // 7226480
+        // => 16384 (the fatmach offset?)
+
+        let codegenmodules_offset = dwarf_data.codegenmodules_offset.unwrap() as usize;
+        let assembly_in_modules =
+            dylib_arch_buf.pread::<u64>(codegenmodules_offset).unwrap() as usize;
+        // assert_eq!(assembly_addr, assembly_in_modules);
+
+        let module = Il2CppCodeGenModule::parse(dylib_arch_buf, assembly_in_modules).unwrap();
+        dbg!(&module);
+
+        for fn_ptr in module.method_pointers {
+            dbg!(fn_ptr, dwarf_data.functions.get(fn_ptr));
+        }
     }
 
     /// Finds the binary offset of the `g_CodeGenModules` symbol
@@ -140,7 +320,20 @@ mod tests {
 
                 if let Some(attr) = entry.attr_value(gimli::constants::DW_AT_name)? {
                     let name = dwarf.attr_string(&unit, attr)?;
+
+                    if name.starts_with(b"NewBehaviourScript") {
+                        println!("{:?}:", std::str::from_utf8(&name));
+                        println!("<{}><{:x}> {}", depth, entry.offset().0, entry.tag());
+
+                        // Iterate over the attributes in the DIE.
+                        let mut attrs = entry.attrs();
+                        while let Some(attr) = attrs.next()? {
+                            println!("   {}: {:?}", attr.name(), attr.value());
+                        }
+                    }
+
                     if name.as_ref() == symbol {
+                        // parse the location
                         if let Some(gimli::AttributeValue::Exprloc(expr)) =
                             entry.attr_value(gimli::constants::DW_AT_location)?
                         {
