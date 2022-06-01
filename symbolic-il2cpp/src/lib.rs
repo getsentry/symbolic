8c75462ffc792316aaf1611228fde72da8f88134
Sebastian Zivota <loewenheim@mailbox.org>
2022-06-01 17:02:06 +0200

    Consolidate into one file

--- symbolic-il2cpp/src/lib.rs
+++ symbolic-il2cpp/src/lib.rs
@@ -1,2 +1,114 @@
-mod line_mapping;
-pub(crate) mod source_info;
+use std::collections::BTreeMap;
+use std::iter::Enumerate;
+use std::str::Lines;
+
+use symbolic_common::ByteView;
+use symbolic_debuginfo::{DebugSession, ObjectLike};
+
+/// The Line Mapping is represented as nested maps like this:
+/// C++ file => C# file => C++ line => C# line
+pub type LineMapping = BTreeMap<String, BTreeMap<String, BTreeMap<u32, u32>>>;
+
+/// An Il2cpp `source_info` record.
+#[non_exhaustive]
+pub struct SourceInfo<'data> {
+    /// The C++ source line the `source_info` was parsed from.
+    pub cpp_line: u32,
+    /// The corresponding C# source file.
+    pub cs_file: &'data str,
+    /// The corresponding C# source line.
+    pub cs_line: u32,
+}
+
+/// An iterator over Il2cpp `source_info` markers.
+///
+/// The Iterator yields `(file, line)` pairs.
+pub struct SourceInfos<'data> {
+    lines: Enumerate<Lines<'data>>,
+}
+
+impl<'data> SourceInfos<'data> {
+    /// Parses the `source` leniently, yielding an empty Iterator for non-utf8 data.
+    pub fn new(source: &'data [u8]) -> Self {
+        let lines = std::str::from_utf8(source)
+            .ok()
+            .unwrap_or_default()
+            .lines()
+            .enumerate();
+        Self { lines }
+    }
+}
+
+impl<'data> Iterator for SourceInfos<'data> {
+    type Item = SourceInfo<'data>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        for (cpp_line, cpp_src_line) in &mut self.lines {
+            match parse_line(cpp_src_line) {
+                Some((cs_file, cs_line)) => {
+                    return Some(SourceInfo {
+                        cpp_line: (cpp_line + 1) as u32,
+                        cs_file,
+                        cs_line,
+                    })
+                }
+                None => continue,
+            }
+        }
+        None
+    }
+}
+
+/// Extracts the `(file, line)` information
+fn parse_line(line: &str) -> Option<(&str, u32)> {
+    let line = line.trim();
+    let source_ref = line.strip_prefix("//<source_info:")?;
+    let source_ref = source_ref.strip_suffix('>')?;
+    let (file, line) = source_ref.rsplit_once(':')?;
+    let line = line.parse().ok()?;
+    Some((file, line))
+}
+
+/// Create a Line Mapping from the given `object`.
+///
+/// The mapping is constructed by iterating over all the source files referenced by `object` and
+/// parsing Il2cpp `source_info` records from each.
+pub fn create_line_mapping_from_object<'data, 'object, O, E>(
+    object: &'object O,
+) -> Result<LineMapping, E>
+where
+    O: ObjectLike<'data, 'object, Error = E>,
+{
+    let session = object.debug_session()?;
+
+    let mut mapping = BTreeMap::new();
+
+    for cpp_file in session.files() {
+        let cpp_file_path = cpp_file?.abs_path_str();
+        if mapping.contains_key(&cpp_file_path) {
+            continue;
+        }
+
+        if let Ok(cpp_source) = ByteView::open(&cpp_file_path) {
+            let mut cpp_mapping = BTreeMap::new();
+
+            for SourceInfo {
+                cpp_line,
+                cs_file,
+                cs_line,
+            } in SourceInfos::new(&cpp_source)
+            {
+                let cs_mapping = cpp_mapping
+                    .entry(cs_file.to_string())
+                    .or_insert_with(BTreeMap::new);
+                cs_mapping.insert(cpp_line, cs_line);
+            }
+
+            if !cpp_mapping.is_empty() {
+                mapping.insert(cpp_file_path, cpp_mapping);
+            }
+        }
+    }
+
+    Ok(mapping)
+}
