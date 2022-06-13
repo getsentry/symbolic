d891f18e3d6e57324923e3c9ea0cd9c9f27b4b3c
Sebastian Zivota <loewenheim@mailbox.org>
2022-06-13 14:24:32 +0200

    Refactor to handle edge cases

--- symbolic-il2cpp/src/line_mapping/from_object.rs
+++ symbolic-il2cpp/src/line_mapping/from_object.rs
@@ -95,9 +95,10 @@ struct SourceInfo<'data> {
 
 /// An iterator over Il2cpp `source_info` markers.
 ///
-/// The Iterator yields `(file, line)` pairs.
+/// The Iterator yields `SourceInfo`s.
 struct SourceInfos<'data> {
     lines: Enumerate<Lines<'data>>,
+    current: Option<(&'data str, u32)>,
 }
 
 impl<'data> SourceInfos<'data> {
@@ -108,7 +109,10 @@ impl<'data> SourceInfos<'data> {
             .unwrap_or_default()
             .lines()
             .enumerate();
-        Self { lines }
+        Self {
+            lines,
+            current: None,
+        }
     }
 }
 
@@ -116,18 +120,30 @@ impl<'data> Iterator for SourceInfos<'data> {
     type Item = SourceInfo<'data>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        while let Some((_, cpp_src_line)) = self.lines.next() {
+        for (cpp_line_nr, cpp_src_line) in &mut self.lines {
             match parse_line(cpp_src_line) {
-                None => continue,
+                // A new line info mapping. Emit the previously found one, if there is one.
                 Some((cs_file, cs_line)) => {
-                    let next_non_comment_line_nr = self.lines.find_map(|(nr, src_line)| {
-                        (!src_line.trim_start().starts_with("//")).then(|| nr)
-                    })?;
-                    return Some(SourceInfo {
-                        cpp_line: (next_non_comment_line_nr + 1) as u32,
-                        cs_file,
-                        cs_line,
-                    });
+                    if let Some((cs_file, cs_line)) = self.current.replace((cs_file, cs_line)) {
+                        return Some(SourceInfo {
+                            cpp_line: cpp_line_nr as u32,
+                            cs_file,
+                            cs_line,
+                        });
+                    }
+                }
+
+                // A comment. Just continue.
+                None if cpp_src_line.trim_start().starts_with("//") => continue,
+                // A source line. Emit the previously found source line mapping, if there is one.
+                None => {
+                    if let Some((cs_file, cs_line)) = self.current.take() {
+                        return Some(SourceInfo {
+                            cpp_line: (cpp_line_nr + 1) as u32,
+                            cs_file,
+                            cs_line,
+                        });
+                    }
                 }
             }
         }
@@ -212,6 +228,37 @@ mod tests {
                     cs_file: "main.cs",
                     cs_line: 46,
                 }
+            ]
+        )
+    }
+
+    #[test]
+    fn missing_source_line() {
+        let cpp_source = b"
+            Lorem ipsum dolor sit amet
+            //<source_info:main.cs:17>
+            // some
+            // comments
+            //<source_info:main.cs:29>
+            actual source code";
+
+        let source_infos: Vec<_> = SourceInfos::new(cpp_source).collect();
+
+        // The first source_info has no source line to attach to, so it should use the line
+        // immediately before the second source_info.
+        assert_eq!(
+            source_infos,
+            vec![
+                SourceInfo {
+                    cpp_line: 5,
+                    cs_file: "main.cs",
+                    cs_line: 17,
+                },
+                SourceInfo {
+                    cpp_line: 7,
+                    cs_file: "main.cs",
+                    cs_line: 29,
+                },
             ]
         )
     }
