bf1100f0fe89ac6716b7fa77e9cf5e340f0e1f29
Sebastian Zivota <loewenheim@mailbox.org>
2022-06-28 12:16:13 +0200

    Emit special ra rule only on MIPS

--- symbolic-cfi/src/lib.rs
+++ symbolic-cfi/src/lib.rs
@@ -273,7 +273,7 @@ impl<U> UnwindInfo<U> {
         let arch = object.arch();
         let load_address = object.load_address();
 
-        // CFI information can have relative offsets to the virtual address of the respective debug
+        // CFI can have relative offsets to the virtual address of the respective debug
         // section (either `.eh_frame` or `.debug_frame`). We need to supply this offset to the
         // entries iterator before starting to interpret instructions. The other base addresses are
         // not needed for CFI.
@@ -746,18 +746,24 @@ impl<W: Write> AsciiCfiWriter<W> {
                 // Print only registers that have changed rules to their previous occurrence to
                 // reduce the number of rules per row. Then, cache the new occurrence for the next
                 // row.
-                let mut did_write_ra = false;
+                let mut ra_written = false;
                 for &(register, ref rule) in row.registers() {
                     if !rule_cache.get(&register).map_or(false, |c| c == &rule) {
                         rule_cache.insert(register, rule);
                         if register == ra {
-                            did_write_ra = true;
+                            ra_written = true;
                         }
                         written |=
                             Self::write_register_rule(&mut line, info.arch, register, rule, ra)?;
                     }
                 }
-                if row.start_address() == start && !did_write_ra {
+                // On MIPS: if no explicit rule was encountered for the return address,
+                // emit a rule stating that the return address should be recovered from the
+                // $ra register.
+                if row.start_address() == start
+                    && !ra_written
+                    && matches!(info.arch, Arch::Mips | Arch::Mips64)
+                {
                     write!(line, " .ra: $ra")?;
                 }
 
