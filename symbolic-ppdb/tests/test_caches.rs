b7541617cdfd979ff4dea996855ac7f99fb8a599
Arpad Borsos <arpad.borsos@sentry.io>
2022-08-12 09:58:28 +0200

    put the Ids into the assertions

--- symbolic-ppdb/tests/test_caches.rs
+++ symbolic-ppdb/tests/test_caches.rs
@@ -1,4 +1,8 @@
+use std::str::FromStr;
+
+use symbolic_common::DebugId;
 use symbolic_common::Language;
+use symbolic_debuginfo::pe::PeObject;
 use symbolic_ppdb::LineInfo;
 use symbolic_ppdb::PortablePdb;
 use symbolic_ppdb::PortablePdbCache;
@@ -99,30 +103,23 @@ fn test_matching_ids() {
 
     let (guid, age) = pdb_id.split_at(16);
     let age = u32::from_ne_bytes(age.try_into().unwrap());
-    let pdb_debug_id = symbolic_common::DebugId::from_guid_age(guid, age).unwrap();
+    let pdb_debug_id = DebugId::from_guid_age(guid, age).unwrap();
 
     let pe_buf = std::fs::read("tests/fixtures/integration.dll").unwrap();
-    let pe = symbolic_debuginfo::pe::PeObject::parse(&pe_buf).unwrap();
+    let pe = PeObject::parse(&pe_buf).unwrap();
     let pe_debug_id = pe.debug_id();
 
     assert_eq!(pe_debug_id, pdb_debug_id);
-}
-
-#[test]
-fn test_pe_metadata() {
-    let pe_buf = std::fs::read("tests/fixtures/integration.dll").unwrap();
-    let pe = symbolic_debuginfo::pe::PeObject::parse(&pe_buf).unwrap();
-
-    let clr_metadata_buf = pe.clr_metadata().unwrap();
-    let metadata = PortablePdb::parse(clr_metadata_buf);
-
-    assert!(metadata.is_ok());
+    assert_eq!(
+        pdb_debug_id,
+        DebugId::from_str("1d6929b4-468b-4db8-9389-9a12bd257e1b-ab8cf31e").unwrap()
+    );
 }
 
 #[test]
 fn test_pe_mvid() {
     let pe_buf = std::fs::read("tests/fixtures/integration.dll").unwrap();
-    let pe = symbolic_debuginfo::pe::PeObject::parse(&pe_buf).unwrap();
+    let pe = PeObject::parse(&pe_buf).unwrap();
 
     let clr_metadata_buf = pe.clr_metadata().unwrap();
     let metadata = PortablePdb::parse(clr_metadata_buf).unwrap();
@@ -131,6 +128,6 @@ fn test_pe_mvid() {
 
     assert_eq!(
         mvid,
-        Some(uuid::uuid!("a4e610cc-ee58-4815-a87f-483e1c6c4ff3"))
+        Some(uuid::uuid!("7d4fc54f-cafa-45ba-a23f-60c8e128f3cf"))
     )
 }
