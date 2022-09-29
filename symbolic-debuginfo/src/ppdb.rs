cb8fc7269762ce5d844717c7dd28b5b6404d8647
Sebastian Zivota <loewenheim@mailbox.org>
2022-09-29 12:12:44 +0200

    docs

--- symbolic-debuginfo/src/ppdb.rs
+++ symbolic-debuginfo/src/ppdb.rs
@@ -1,3 +1,4 @@
+//! Support for Portable PDB Objects.
 use std::convert::Infallible;
 use std::iter;
 
@@ -6,11 +7,15 @@ use symbolic_ppdb::{FormatError, PortablePdb};
 
 use crate::{DebugSession, FileEntry, Function, ObjectKind, Parse, Symbol, SymbolMap};
 
+/// An iterator over symbols in a [`PortablePdbObject`].
 pub type PortablePdbSymbolIterator = iter::Empty<Symbol<'static>>;
+/// An iterator over functions in a [`PortablePdbObject`].
 pub type PortablePdbFunctionIterator<'session> =
     iter::Empty<Result<Function<'session>, Infallible>>;
+/// An iterator over files in a [`PortablePdbObject`].
 pub type PortablePdbFileIterator<'session> = iter::Empty<Result<FileEntry<'session>, Infallible>>;
 
+/// An object wrapping a Portable PDB file.
 #[derive(Debug)]
 pub struct PortablePdbObject<'data> {
     data: &'data [u8],
@@ -18,59 +23,81 @@ pub struct PortablePdbObject<'data> {
 }
 
 impl<'data> PortablePdbObject<'data> {
-    pub fn debug_id(&self) -> DebugId {
-        // TODO: How to handle nonexistent pdb_ids?
-        self.ppdb.pdb_id().unwrap()
+    /// Returns the Portable PDB contained in this object.
+    pub fn portable_pdb(&self) -> &PortablePdb {
+        &self.ppdb
     }
 
+    /// The debug information identifier of a Portable PDB file.
+    pub fn debug_id(&self) -> DebugId {
+        self.ppdb.pdb_id().unwrap_or_default()
+    }
+
+    /// The code identifier of this object.
+    ///
+    /// Portable PDB does not provide code identifiers.
     pub fn code_id(&self) -> Option<CodeId> {
         None
     }
 
+    /// The CPU architecture of this object.
     pub fn arch(&self) -> Arch {
         Arch::Unknown
     }
 
+    /// The kind of this object.
     pub fn kind(&self) -> ObjectKind {
         ObjectKind::Debug
     }
 
+    /// The address at which the image prefers to be loaded into memory.
+    ///
+    /// This is always 0 as this does not really apply to Portable PDB.
     pub fn load_address(&self) -> u64 {
         0
     }
 
+    /// Returns true if this object exposes a public symbol table.
     pub fn has_symbols(&self) -> bool {
         false
     }
 
+    /// Returns an iterator over symbols in the public symbol table.
     pub fn symbols(&self) -> PortablePdbSymbolIterator {
         iter::empty()
     }
 
+    /// Returns an ordered map of symbols in the symbol table.
     pub fn symbol_map(&self) -> SymbolMap<'data> {
         SymbolMap::new()
     }
 
+    /// Determines whether this object contains debug information.
     pub fn has_debug_info(&self) -> bool {
         self.ppdb.has_debug_info()
     }
 
+    /// Constructs a debugging session.
     pub fn debug_session(&self) -> Result<PortablePdbDebugSession, Infallible> {
         Ok(PortablePdbDebugSession)
     }
 
+    /// Determines whether this object contains stack unwinding information.
     pub fn has_unwind_info(&self) -> bool {
         false
     }
 
+    /// Determines whether this object contains embedded source.
     pub fn has_sources(&self) -> bool {
         false
     }
 
+    /// Determines whether this object is malformed and was only partially parsed.
     pub fn is_malformed(&self) -> bool {
         false
     }
 
+    /// Returns the raw data of the Portable PDB file.
     pub fn data(&self) -> &'data [u8] {
         self.data
     }
@@ -89,6 +116,9 @@ impl<'data> Parse<'data> for PortablePdbObject<'data> {
     }
 }
 
+/// A debug session for a Portable PDB object.
+///
+/// Currently this session is trivial and returns no files or functions.
 pub struct PortablePdbDebugSession;
 
 impl<'session> DebugSession<'session> for PortablePdbDebugSession {
