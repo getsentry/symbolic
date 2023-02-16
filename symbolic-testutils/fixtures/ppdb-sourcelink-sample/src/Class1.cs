51a2e90b324d2e4b368e646d48048fe9dc3e4b35
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2023-02-16 08:55:10 -0800

    Add sources for PPDB SourceLink sample

---
+++ symbolic-testutils/fixtures/ppdb-sourcelink-sample/src/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sample
+{
+    public class Class1
+    {
+        // Just a test
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
