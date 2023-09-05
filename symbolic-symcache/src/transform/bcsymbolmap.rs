c691be458a9e33ced11b0734856e4d4802280503
Sebastian Zivota <loewenheim@mailbox.org>
2023-09-05 17:34:33 +0200

    fix: Clippy

--- symbolic-symcache/src/transform/bcsymbolmap.rs
+++ symbolic-symcache/src/transform/bcsymbolmap.rs
@@ -6,38 +6,27 @@ use symbolic_debuginfo::macho::BcSymbolMap;
 
 use super::{File, Function, SourceLocation, Transformer};
 
-// This ended up as a macro which "inlines" mapping the `Cow` into the calling function, as using
-// a real function here would lead to the following borrow checker error:
-// error[E0495]: cannot infer an appropriate lifetime for lifetime parameter `'d` due to conflicting requirements
-macro_rules! map_cow {
-    ($cow:expr, $f: expr) => {
-        match $cow {
-            Cow::Borrowed(inner) => Cow::Borrowed($f(inner)),
-            Cow::Owned(inner) => Cow::Owned($f(&inner).to_owned()),
-        }
-    };
+fn resolve_cow<'f>(map: &'f BcSymbolMap<'_>, s: Cow<'f, str>) -> Cow<'f, str> {
+    match s {
+        Cow::Borrowed(inner) => Cow::Borrowed(map.resolve(inner)),
+        Cow::Owned(inner) => Cow::Owned(map.resolve(&inner).to_owned()),
+    }
 }
 
 impl Transformer for BcSymbolMap<'_> {
     fn transform_function<'f>(&'f mut self, f: Function<'f>) -> Function<'f> {
         Function {
-            name: map_cow!(f.name, |s| self.resolve(s)),
-            comp_dir: f.comp_dir.map(|dir| map_cow!(dir, |s| self.resolve(s))),
+            name: resolve_cow(self, f.name),
+            comp_dir: f.comp_dir.map(|dir| resolve_cow(self, dir)),
         }
     }
 
     fn transform_source_location<'f>(&'f mut self, sl: SourceLocation<'f>) -> SourceLocation<'f> {
         SourceLocation {
             file: File {
-                name: map_cow!(sl.file.name, |s| self.resolve(s)),
-                directory: sl
-                    .file
-                    .directory
-                    .map(|dir| map_cow!(dir, |s| self.resolve(s))),
-                comp_dir: sl
-                    .file
-                    .comp_dir
-                    .map(|dir| map_cow!(dir, |s| self.resolve(s))),
+                name: resolve_cow(self, sl.file.name),
+                directory: sl.file.directory.map(|dir| resolve_cow(self, dir)),
+                comp_dir: sl.file.comp_dir.map(|dir| resolve_cow(self, dir)),
             },
             line: sl.line,
         }
