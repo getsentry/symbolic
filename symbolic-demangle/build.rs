50a7758c2d2266a41833b35e7bc9cc0d06fcb21c
David Herberth <david.herberth@sentry.io>
2025-01-09 11:33:40 +0100

    fix linux build, maybe

--- symbolic-demangle/build.rs
+++ symbolic-demangle/build.rs
@@ -21,8 +21,9 @@ fn main() {
             .flag_if_supported("-mmacosx-version-min=11.0.0")
             .flag("-DLLVM_DISABLE_ABI_BREAKING_CHECKS_ENFORCING=1")
             .flag("-DSWIFT_STDLIB_HAS_TYPE_PRINTING=1")
+            .flag("-Wchanges-meaning")
             .warnings(false)
             .include("vendor/swift/include")
-            .compile("swiftdemangleabc");
+            .compile("swiftdemangle");
     }
 }
