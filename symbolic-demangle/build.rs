d8bf4067ab818d19619a2207f79b4c59b87b0ba8
David Herberth <david.herberth@sentry.io>
2025-01-09 11:23:49 +0100

    remove unused sources

--- symbolic-demangle/build.rs
+++ symbolic-demangle/build.rs
@@ -12,19 +12,10 @@ fn main() {
                 "vendor/swift/lib/Demangling/ManglingUtils.cpp",
                 "vendor/swift/lib/Demangling/NodeDumper.cpp",
                 "vendor/swift/lib/Demangling/NodePrinter.cpp",
-                "vendor/swift/lib/Demangling/OldDemangler.cpp",
-                "vendor/swift/lib/Demangling/OldRemangler.cpp",
+                // "vendor/swift/lib/Demangling/OldDemangler.cpp",
+                // "vendor/swift/lib/Demangling/OldRemangler.cpp",
                 "vendor/swift/lib/Demangling/Punycode.cpp",
                 "vendor/swift/lib/Demangling/Remangler.cpp",
-                // "vendor/swift/lib/Demangling/Demangler.cpp",
-                // "vendor/swift/lib/Demangling/Context.cpp",
-                // "vendor/swift/lib/Demangling/ManglingUtils.cpp",
-                // "vendor/swift/lib/Demangling/NodeDumper.cpp",
-                // "vendor/swift/lib/Demangling/NodePrinter.cpp",
-                // "vendor/swift/lib/Demangling/OldDemangler.cpp",
-                // // "vendor/swift/lib/Demangling/OldRemangler.cpp",
-                // "vendor/swift/lib/Demangling/Punycode.cpp",
-                // "vendor/swift/lib/Demangling/Remangler.cpp",
             ])
             .flag_if_supported("-std=c++17")
             .flag_if_supported("-mmacosx-version-min=11.0.0")
