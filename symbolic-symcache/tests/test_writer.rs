9ac5c8656dff9932fcff1ad7d7323abe91e65bb5
Sebastian Zivota <loewenheim@mailbox.org>
2025-02-11 10:37:46 +0100

    Fix tests

--- symbolic-symcache/tests/test_writer.rs
+++ symbolic-symcache/tests/test_writer.rs
@@ -23,7 +23,7 @@ fn test_write_header_linux() -> Result<(), Error> {
     }
 
     let symcache = SymCache::parse(&buffer)?;
-    insta::assert_debug_snapshot!(symcache, @r#"
+    insta::assert_debug_snapshot!(symcache, @r###"
     SymCache {
         version: 8,
         debug_id: DebugId {
@@ -33,11 +33,11 @@ fn test_write_header_linux() -> Result<(), Error> {
         arch: Amd64,
         files: 55,
         functions: 697,
-        source_locations: 8284,
-        ranges: 6828,
+        source_locations: 8431,
+        ranges: 6975,
         string_bytes: 49877,
     }
-    "#);
+    "###);
 
     Ok(())
 }
@@ -67,7 +67,7 @@ fn test_write_header_macos() -> Result<(), Error> {
     converter.process_object(&object)?;
     converter.serialize(&mut Cursor::new(&mut buffer))?;
     let symcache = SymCache::parse(&buffer)?;
-    insta::assert_debug_snapshot!(symcache, @r#"
+    insta::assert_debug_snapshot!(symcache, @r###"
     SymCache {
         version: 8,
         debug_id: DebugId {
@@ -77,11 +77,11 @@ fn test_write_header_macos() -> Result<(), Error> {
         arch: Amd64,
         files: 36,
         functions: 639,
-        source_locations: 7199,
-        ranges: 5782,
+        source_locations: 7382,
+        ranges: 5965,
         string_bytes: 40958,
     }
-    "#);
+    "###);
 
     Ok(())
 }
