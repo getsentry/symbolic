44694e9570cbf1d1226037e6d670eaf51b8a7202
Alexander Weber <alexander.weber@sentry.io>
2025-07-18 09:27:56 +0200

    build: switch to python3.11 for releases builds

--- py/manylinux.sh
+++ py/manylinux.sh
@@ -5,21 +5,21 @@ set -e
 yum -y -q -e 0 install gcc libffi-devel
 
 # upgrade wheel
-/opt/python/cp36-cp36m/bin/pip install --upgrade wheel
+/opt/python/cp311-cp311/bin/pip install --upgrade wheel
 
 # Install Rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH=~/.cargo/bin:$PATH
 
-cat > ~/.cargo/config <<EOF
+cat >~/.cargo/config <<EOF
 [net]
 git-fetch-with-cli = true
 EOF
 
-/opt/python/cp36-cp36m/bin/python setup.py bdist_wheel
+/opt/python/cp311-cp311/bin/python setup.py bdist_wheel
 
 # Audit wheels
 for wheel in dist/*-linux_*.whl; do
-  auditwheel repair "$wheel" -w dist/
-  rm "$wheel"
+	auditwheel repair "$wheel" -w dist/
+	rm "$wheel"
 done
