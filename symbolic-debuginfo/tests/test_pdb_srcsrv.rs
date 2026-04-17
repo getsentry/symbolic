5cbc898a70e5dce93f3dce979a3d7a786352f871
Sebastian Zivota <loewenheim@mailbox.org>
2026-04-17 14:15:29 +0200

    Simplify tests

--- symbolic-debuginfo/tests/test_pdb_srcsrv.rs
+++ symbolic-debuginfo/tests/test_pdb_srcsrv.rs
@@ -1,7 +1,7 @@
-//! Tests for PDB SRCSRV path remapping functionality
+//! Tests for PDB SRCSRV functionality
 //!
 //! These tests verify that source server (SRCSRV) information embedded in PDB files
-//! is properly parsed and used to remap file paths. This is commonly used in game
+//! is properly parsed and used to fill in file information. This is commonly used in game
 //! development where builds happen on different machines with Perforce.
 
 use symbolic_common::ByteView;
@@ -9,134 +9,76 @@ use symbolic_debuginfo::pdb::PdbObject;
 use symbolic_debuginfo::Object;
 use symbolic_testutils::fixture;
 
-type Error = Box<dyn std::error::Error>;
-
 #[test]
-fn test_pdb_srcsrv_vcs_name() -> Result<(), Error> {
-    let view = ByteView::open(fixture("windows/crash_with_srcsrv.pdb"))?;
-    let pdb = PdbObject::parse(&view)?;
+fn test_pdb_srcsrv_vcs_name() {
+    let view = ByteView::open(fixture("windows/crash_with_srcsrv.pdb")).unwrap();
+    let pdb = PdbObject::parse(&view).unwrap();
 
     // This PDB file contains Perforce SRCSRV information
-    let vcs_name = pdb.debug_session()?.srcsrv_vcs_name();
+    let vcs_name = pdb.debug_session().unwrap().srcsrv_vcs_name();
     assert_eq!(vcs_name, Some("Perforce".to_string()));
-
-    Ok(())
 }
 
 #[test]
-fn test_pdb_files_are_remapped() -> Result<(), Error> {
-    let view = ByteView::open(fixture("windows/crash_with_srcsrv.pdb"))?;
-    let object = Object::parse(&view)?;
+fn test_pdb_file_info() {
+    let view = ByteView::open(fixture("windows/crash_with_srcsrv.pdb")).unwrap();
+    let object = Object::parse(&view).unwrap();
 
-    // Get the debug session which will parse SRCSRV and remap paths
-    let session = object.debug_session()?;
+    let session = object.debug_session().unwrap();
 
-    // Collect all file entries from the PDB
-    let mut found_expected = false;
-    for file_result in session.files() {
-        let file_entry = file_result?;
-        let Some(path) = file_entry.srcsrv_path_str() else {
-            continue;
-        };
+    let file = session
+        .files()
+        .map(|file| file.unwrap())
+        .find(|file| {
+            // Expected specific path based on the SRCSRV data in the test PDB:
+            // c:\projects\breakpad-tools\deps\breakpad\src\client\windows\crash_generation\crash_generation_client.cc
+            //   -> depot/breakpad/src/client/windows/crash_generation/crash_generation_client.cc (path)
+            //   -> 12345 (revision)
+            file.srcsrv_path_str().as_deref()
+                == Some(
+                    "depot/breakpad/src/client/windows/crash_generation/crash_generation_client.cc",
+                )
+        })
+        .unwrap();
 
-        // Expected specific path based on the SRCSRV data in the test PDB:
-        // c:\projects\breakpad-tools\deps\breakpad\src\client\windows\crash_generation\crash_generation_client.cc
-        //   -> depot/breakpad/src/client/windows/crash_generation/crash_generation_client.cc (path)
-        //   -> 12345 (revision)
-        if path == "depot/breakpad/src/client/windows/crash_generation/crash_generation_client.cc" {
-            // Verify the revision is set correctly
-            let revision = file_entry.srcsrv_revision();
-            assert_eq!(revision, Some("12345"));
-            found_expected = true;
-            break;
-        }
-    }
-
-    assert!(
-        found_expected,
-        "Expected to find remapped path with revision: depot/breakpad/.../crash_generation_client.cc"
-    );
-
-    Ok(())
+    assert_eq!(file.srcsrv_revision(), Some("12345"));
 }
 
 #[test]
-fn test_pdb_functions_are_remapped() -> Result<(), Error> {
-    let view = ByteView::open(fixture("windows/crash_with_srcsrv.pdb"))?;
-    let object = Object::parse(&view)?;
+fn test_pdb_line_info() {
+    let view = ByteView::open(fixture("windows/crash_with_srcsrv.pdb")).unwrap();
+    let object = Object::parse(&view).unwrap();
 
-    // Get the debug session which will parse SRCSRV and remap paths
-    let session = object.debug_session()?;
+    let session = object.debug_session().unwrap();
 
-    // Find functions with the expected remapped file
-    let mut found_expected = false;
-    for func_result in session.functions() {
-        let func = func_result?;
-        for line in &func.lines {
-            let Some(path) = line.file.srcsrv_path_str() else {
-                continue;
-            };
-
+    let line = session
+        .functions()
+        .map(|function| function.unwrap())
+        .flat_map(|function| function.lines.into_iter())
+        .find(|line| {
             // Expected specific path based on the SRCSRV data in the test PDB
             // Path should NOT contain @12345, revision should be separate
-            if path
-                == "depot/breakpad/src/client/windows/crash_generation/crash_generation_client.cc"
-            {
-                // Verify the revision is set correctly
-                let revision = line.file.srcsrv_revision();
-                assert_eq!(revision, Some("12345"),);
-                found_expected = true;
-                break;
-            }
-        }
-        if found_expected {
-            break;
-        }
-    }
+            line.file.srcsrv_path_str().as_deref()
+                == Some(
+                    "depot/breakpad/src/client/windows/crash_generation/crash_generation_client.cc",
+                )
+        })
+        .unwrap();
 
-    assert!(
-        found_expected,
-        "Expected to find remapped path with revision in functions"
-    );
-
-    Ok(())
+    assert_eq!(line.file.srcsrv_revision(), Some("12345"),);
 }
 
 #[test]
-fn test_pdb_without_srcsrv() -> Result<(), Error> {
-    // Test with a regular PDB that doesn't have SRCSRV data
-    let view = ByteView::open(fixture("windows/crash.pdb"))?;
-    let pdb = PdbObject::parse(&view)?;
+fn test_pdb_without_srcsrv() {
+    let view = ByteView::open(fixture("windows/crash.pdb")).unwrap();
+    let pdb = PdbObject::parse(&view).unwrap();
 
-    // This PDB file does not contain SRCSRV information
-    let vcs_name = pdb.debug_session()?.srcsrv_vcs_name();
+    let vcs_name = pdb.debug_session().unwrap().srcsrv_vcs_name();
     assert_eq!(vcs_name, None);
 
-    // Should return None for PDBs without SRCSRV
-    let srcsrv_data = pdb.has_source_server_data()?;
+    let srcsrv_data = pdb.has_source_server_data().unwrap();
     assert!(
         !srcsrv_data,
         "Regular PDB without SRCSRV should return None"
     );
-
-    // Parsing should still work, just without path remapping
-    let object = Object::parse(&view)?;
-    let session = object.debug_session()?;
-
-    // Should still be able to iterate files (just without remapping)
-    let mut file_count = 0;
-    for file_result in session.files() {
-        file_result?;
-        file_count += 1;
-        if file_count >= 5 {
-            break; // Just verify a few files work
-        }
-    }
-
-    assert!(
-        file_count > 0,
-        "Should be able to read files from regular PDB"
-    );
-
-    Ok(())
 }
