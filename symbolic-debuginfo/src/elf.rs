b39cf4b23d04f3b30d5376118b154e0e225792af
David Herberth <david.herberth@sentry.io>
2026-07-07 10:45:22 +0200

    require a dot

--- symbolic-debuginfo/src/elf.rs
+++ symbolic-debuginfo/src/elf.rs
@@ -671,10 +671,11 @@ impl<'data> ElfObject<'data> {
                 // Support this as an override to the flag.
                 let (compressed, section_name) = match section_name.strip_prefix(".z") {
                     Some(name) => (true, name),
-                    None => (
-                        header.sh_flags & SHF_COMPRESSED != 0,
-                        section_name.strip_prefix('.').unwrap_or(section_name),
-                    ),
+                    None => match section_name.strip_prefix('.') {
+                        Some(name) => (header.sh_flags & SHF_COMPRESSED != 0, name),
+                        // We expect any section we look for to actually start with a `.`.
+                        None => continue,
+                    },
                 };
 
                 if section_name != name {
